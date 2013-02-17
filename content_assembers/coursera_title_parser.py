bfea6fe971ab41d82d23af4367e2a0838c27729d
zaqwes <igor.a.lugansky@gmail.com>
2013-02-17 19:14:19 +0400

    Think about recursive algoritm.

--- content_assembers/coursera_title_parser.py
+++ content_assembers/coursera_title_parser.py
@@ -12,14 +12,30 @@ import uasio.os_io.io_wrapper as iow
 import _http_requester as http_request
 
 def _split_url(url):
+    """ http://www.dessci.com/en/products/mathplayer/ to 
+    www.dessci.com /en/products/mathplayer/ - для httplib
+    """
     url_copy = url.replace('https://','')
     url_copy = url_copy.replace('http://','')
     
     head = url_copy.split('/')[0]
     path = url_copy[len(head):]
-    return path#head#url_copy
+    return head+' '+path
+    
+def _iteration():
+    foldered_list = ('@@@'.join(lst)).split(levels[0])
+    for it in foldered_list:
+        splitted = it.split('@@@')
+        if splitted[0]:
+            # заполняем
+            result_dict[splitted[0]] = splitted[1:]
+                
+def _fill_tree_dict(lst, levels, level_pos, result_dict):
+    result_dict = {}
+  
+    return result_dict
 
-if __name__=='__main__':
+def main():
     fname = 'lessions_names.txt'
     fname = 'lessions_html_code.txt'
     
@@ -27,25 +43,35 @@ if __name__=='__main__':
     sets['name'] = fname
     
     i = 0
-    result_dict = {}
+    result_dict = []
     readed_list = iow.file2list(sets)
    
     for at in readed_list:
+        # Предварительная фильтарция
+        at = at.replace('</a>', '')
+        
+        # Ссылки с содержанием
         if 'pdf' in at or '&format=srt' in at:
             at_copy = at.replace('    <a target="_new" href=', '')
-            result_dict[i] = _split_url(at_copy)
+            result_dict.append('link_to_get '+_split_url(at_copy))
 
+        # Темы
         if 'min)' in at and 'div' not in at:
-            result_dict[i] = at
+            result_dict.append('name_content '+at)
             
+        # Части-недели
         if '(Week' in at:
             at_copy_list = at.split('&nbsp;')[1].split('</h3>')[0]
-            print at_copy_list
-            result_dict[i] = at_copy_list
+            result_dict.append('folder '+at_copy_list)
         i += 1
     
+    # теперь нужно запаковать в словарь
+    levels = ['folder', 'name_content', 'link_to_get']
+    result = {}
+    result = _fill_tree_dict(result_dict, levels, len(levels), result)
+    
     # сохраняем результаты в файл
-    to_file = [json.dumps(result_dict, sort_keys=True, indent=2)]
+    to_file = [json.dumps(result, sort_keys=True, indent=2)]
     
     settings = {
         'name':  'extracted_from_html.json', 
@@ -53,5 +79,8 @@ if __name__=='__main__':
         'coding': 'cp866' }
         
     iow.list2file(settings, to_file)
+    
+if __name__=='__main__':
+    main()
     
     print 'Done'
