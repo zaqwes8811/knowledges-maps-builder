000136fa77547dc5c08800329bbc2f952dcd004f
zaqwes <igor.a.lugansky@gmail.com>
2013-03-19 19:29:39 +0400

    fix

---
+++ src/app.py
@@ -0,0 +1,17 @@
+#java -jar D:\work-libs\jython.jar %1
+import sys
+
+# Other
+import dals.os_io.io_wrapper as dal
+
+# App
+import business.analysers as runner
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
+
+
