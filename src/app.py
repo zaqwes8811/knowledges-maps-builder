abd2516b8ae65fc5a2879b0d98fe10b8516d1559
zaqwes <igor.a.lugansky@gmail.com>
2013-03-23 17:43:45 +0400

    Introduce processing into IndexCoursor

--- src/app.py
+++ src/app.py
@@ -1,13 +1,9 @@
 # coding: utf-8
 
-# Other
-import os
-import json
-import dals.os_io.io_wrapper as dal
-
 # App
 import business.originator_frequency_index.orginator as orginator
 from business.to_text import get_list_content_items_from_str 
+from business.originator_frequency_index.orginator import IndexCursor
 
 def get_addrs():
     files = [
@@ -15,81 +11,22 @@ def get_addrs():
             '../statistic_data/srts/Iron Man AA/Iron1and8.srt']
     return files
 
-class IndexCursor(object):
-    """ Итератрор индекса. Работает с одной веткой, но знает 
-        конерь хранилища и его план
-        
-        Thinks:
-            Как будет происходит первое подкл. к индексу? Мы не знаем
-            есть ли ветки
-    """
-    _index_root = ''
-    _own_index = {}  # ядро системы - кэш
-    _current_branch = None
-    
-    def _get_real_branch_name(self):
-        return self._
-    
-    def __init__(self, index_root, init_branch=None):
-        self._index_root = index_root
-    
-    def load_index(self):
-        pass
-    
-    def assign_branch(self, branch_name):
-        self._current_branch = branch_name
-    
-    def do_branch(self, branch_name):
-        # ! Провеить на входимость, иначе затрет!
-        self._own_index = {branch_name:{}}
-        try:
-            os.mkdir(self._index_root+'/'+branch_name.replace(' ', '$$'))
-        except OSError as e:
-            print 'Branch is exist', e
-            
-    def get_branch(self, branch_name):
-        return self._own_index[branch_name]
-    
-    def print_branch(self, branch_name):
-        branch = self._own_index[branch_name]
-        for at in branch:
-            if branch[at]['num'] > 1:
-                print branch[at]['num'], at
-                
-    def get_map(self):
-        print
-        print "IndexCursor map"
-        for at in self._own_index:
-            print at
-    
-    def save_branch(self, branch_name):
-        to_file = [json.dumps(self._own_index, sort_keys=True, indent=2)]
-        sets = dal.get_utf8_template()
-        sets['name'] = 'index.json'
-        sets['howOpen'] = 'w'
-        dal.list2file(sets, to_file)
-        
-    def save_all(self):
-        pass
-    
-    def get_list_nodes(self):
-        list_nodes = os.listdir(self._index_root)
-        result = []
-        for at in list_nodes:
-            result.append(at.replace('$$', ' '))
-        return result
+
 
 def main():
     # Просматриваем индекс
     index_root = 'indexes'
     index = IndexCursor(index_root)
     list_nodes = index.get_list_nodes()
+    print list_nodes
     
-    #index.do_branch(content_item_name)
+    # Типа выбрали ветку
+    content_item_name = 'Iron Man AA'
+    
+    index.assign_branch(content_item_name)
     
     # Получаем пути к субтитрам
     # Сборщик контента - отдельный объект!
-    content_item_name = 'Iron Man AA'
     files = get_addrs()
     
     
--- src/business/originator_frequency_index/orginator.py
+++ src/business/originator_frequency_index/orginator.py
@@ -11,6 +11,9 @@
 # Sys
 
 # Other - не все входят в поставку jython
+import os
+import json
+import dals.os_io.io_wrapper as dal
 
 # App
 from business.to_text import is_content_nums 
@@ -30,26 +33,81 @@ def _is_key_enabled(key_value):
         return False
     return True
 
-
-def process_list_content_sentences(sentences_lst, IndexCursor):
-    # Обработка
-    for one_sentence in sentences_lst:
-        if one_sentence:
-            pure_sentence = _purge_one_sentence(one_sentence)
-            
-            # Лексические единицы одного предложения
-            set_words = pure_sentence.split(' ')
-            for at in set_words:
-                if at != ' ' and at:
-                    if at in IndexCursor:
-                        #if IndexCursor[at]['num'] < _kCountContentItems+1:
-                        #    IndexCursor[at]['sents'].append(one_sentence)
-                        IndexCursor[at]['num'] += 1
-                    else:
-                        # Первое включение
-                        if _is_key_enabled(at):
-                            IndexCursor[at] = {'num':1 }#, 'sents':[one_sentence]}
-
+class IndexCursor(object):
+    """ Итератрор индекса. Работает с одной веткой, но знает 
+        конерь хранилища и его план
+        
+        Thinks:
+            Как будет происходит первое подкл. к индексу? Мы не знаем
+            есть ли ветки
+    """
+    _index_root = ''
+    _branch_cash = {}  # ядро системы - кэш
+    _current_branch = None
+    
+    def _get_real_branch_name(self):
+        #return self._
+        pass
+    
+    def __init__(self, index_root, init_branch=None):
+        self._index_root = index_root
+      
+    def assign_branch(self, branch_name):
+        """ Соединяет курсор с узлом. Если узла нет, создается."""
+        self._current_branch = branch_name
+        
+        try:
+            os.mkdir(self._index_root+'/'+branch_name.replace(' ', '$$'))
+        except OSError as e:
+            print 'Branch is exist', e    
+            # Загружаем индекс 
+    
+    def print_branch(self, branch_name):
+        branch = self._branch_cash[branch_name]
+        for at in branch:
+            if branch[at]['num'] > 1:
+                print branch[at]['num'], at
+                
+    def get_map(self):
+        print
+        print "IndexCursor map"
+        for at in self._branch_cash:
+            print at
+    
+    def save_branch(self, branch_name):
+        to_file = [json.dumps(self._branch_cash, sort_keys=True, indent=2)]
+        sets = dal.get_utf8_template()
+        sets['name'] = 'index.json'
+        sets['howOpen'] = 'w'
+        dal.list2file(sets, to_file)
+    
+    def get_list_nodes(self):
+        list_nodes = os.listdir(self._index_root)
+        result = []
+        for at in list_nodes:
+            result.append(at.replace('$$', ' '))
+        return result
+    
+    # Processors
+    def process_list_content_sentences(self, sentences_lst):
+        """ Получает список предложений и заполняет ветку индекса. """
+        # Обработка
+        for one_sentence in sentences_lst:
+            if one_sentence:
+                pure_sentence = _purge_one_sentence(one_sentence)
+                
+                # Лексические единицы одного предложения
+                set_words = pure_sentence.split(' ')
+                for at in set_words:
+                    if at != ' ' and at:
+                        if at in self._branch_cash:
+                            #if IndexCursor[at]['num'] < _kCountContentItems+1:
+                            #    IndexCursor[at]['sents'].append(one_sentence)
+                            self._branch_cash[at]['num'] += 1
+                        else:
+                            # Первое включение
+                            if _is_key_enabled(at):
+                                self._branch_cash[at] = {'num':1 }#, 'sents':[one_sentence]}
 
 
 if __name__ == '__main__':
