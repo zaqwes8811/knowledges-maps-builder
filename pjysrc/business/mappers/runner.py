bbd90e9c0070e609588f62fe324c5a26f30785cc
zaqwes <igor.a.lugansky@gmail.com>
2013-04-01 22:43:41 +0400

    fix

--- pjysrc/business/mappers/runner.py
+++ pjysrc/business/mappers/runner.py
@@ -11,18 +11,28 @@ import json
 #from business.to_text import get_list_content_items_from_str 
 from business.originators_text_data.srt_to_text import srt_to_text_line
 
-def get_addrs():
+def get_scheme_actions():
     # TODO(zaqwes): Во что сереализуется указатель на функцию - Нельзя его сереализовать
     # Можно подставить имя
-    files = [
-             ['../../../statistic_data/srts/Iron Man AA/Iron Man02x26.srt', 
-                srt_to_text_line, None],
-             ['../../../statistic_data/srts/Iron Man AA/Iron1and8.srt', 
-                srt_to_text_line, None]]
-    return files
+    
+    def one_node_action_fake():
+        files = [
+                 ['../../../statistic_data/srts/Iron Man AA/Iron Man02x26.srt', 
+                    srt_to_text_line, None],
+                 ['../../../statistic_data/srts/Iron Man AA/Iron1and8.srt', 
+                    srt_to_text_line, None]]
+        return files
+    
+    node_name1 = 'Iron Man AA1'
+    node_name2 = 'Iron Man AA2'
+    readed_data = {
+          node_name1: one_node_action_fake(),
+          node_name2: one_node_action_fake()}
+    return readed_data
 
 def mapper(url):
     key = url[0]
+    result = {key: []}
     def print_pathes(item):
         path = item[0]
         
@@ -30,32 +40,36 @@ def mapper(url):
         text = item[1](path)
         
         # теперь можно раздробить на единицы контента
-        content_items = []
-        if item[2]:
-            content_items = item[2](text)
-        else:
-            content_items = [text]
+        #content_items = []
+        #if item[2]:
+            #content_items = item[2](text)
+        #else:
+            #content_items = [text]
 
         # Теперь можно составлять индекс
-        print key
+        result[key].append([text, item[2]])
+        
         
     map(print_pathes, url[1])
+    return result
 
 def main():
-    # Просматриваем индекс
-   
-    # Заготовка путей
-    node_name1 = 'Iron Man AA1'
-    node_name2 = 'Iron Man AA2'
-    
-    readed_data = {
-          node_name1: get_addrs(),
-          node_name2: get_addrs()}
+    # План действий
+    readed_data = get_scheme_actions()
 
     
     # Получить индекс 
     print 'Process files. Wait please...'
-    print map(mapper, readed_data.items())
+    map_result = map(mapper, readed_data.items())
+    print 'Extract text done.'
+    print
+    print 'Plotting results...'
+    
+    def printer(value):
+        for at in value:
+            for it in value[at]:
+                print it    
+    map(printer, map_result)
     
     # Выводим
     #index.print_branch(content_item_name)
