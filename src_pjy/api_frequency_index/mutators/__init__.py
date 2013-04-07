735e42767455fe829655bbd7b62556ed94e3b9a6
zaqwes <igor.a.lugansky@gmail.com>
2013-04-07 09:12:14 +0400

    fix

--- src_pjy/api_frequency_index/mutators/__init__.py
+++ src_pjy/api_frequency_index/mutators/__init__.py
@@ -1,4 +1,15 @@
 # coding: utf-8
 
-class WriteCursor(object):
-    pass
+# Other
+import dals.os_io.io_wrapper as iow
+
+class CursorWriter(object):
+    def __init__(self, index_folder):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    index_head = '../../index'
+    cursor_writer = CursorWriter()
+    print 'Done'
