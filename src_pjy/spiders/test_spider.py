4b730dd84957836b64dbfdf2e47b28d102615d1c
bb <bb@gmail.com>
2013-04-10 19:39:07 +0400

    do processor files with error

--- src_pjy/spiders/test_spider.py
+++ src_pjy/spiders/test_spider.py
@@ -6,12 +6,24 @@ Created on 10.04.2013
 '''
 import unittest
 
+# 
+import dals.os_io.io_wrapper as dal
+
+def parser_target_for_spider(target_fname):
+    sets = dal.get_utf8_template()
+    sets['name'] = target_fname
+    print target_fname
+    list_lines, err = dal.efile2list(sets)
+    if err[0]:
+        return None, err
 
 class Test(unittest.TestCase):
 
 
     def testName(self):
-        print "Hello"
+        target_fname = 'test_spider_target.txt_f'
+        result, err = parser_target_for_spider(target_fname)
+        self.assertIsNone(result, "File no exist")
 
 
 if __name__ == "__main__":
