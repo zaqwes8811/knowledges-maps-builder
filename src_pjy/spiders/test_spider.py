5c19b53e396754fbc342ba7ddfbd36f443c2e639
bb <bb@gmail.com>
2013-04-10 22:30:46 +0400

    Parser done. Need done spider

--- src_pjy/spiders/test_spider.py
+++ src_pjy/spiders/test_spider.py
@@ -28,7 +28,7 @@ def parser_target_for_spider(target_fname):
     list_lines, err = dal.efile2list(sets)
     if err[0]:
         yield None, err
-        raise StopIteration
+        return
     
     # Utils
     remove_forward_and_back_spaces = lambda line: \
@@ -49,7 +49,7 @@ def parser_target_for_spider(target_fname):
     # В первой информационной строке должно быть имя узла
     if not is_node(result_job_list[0]):
         yield None, [2, 'Неверный формат файла - первое имя узла должно быть до адресов.']
-        raise StopIteration
+        return
     
     get_node_name = lambda src_node_name: remove_forward_and_back_spaces(
                                         src_node_name.replace('[', '').replace(']', ''))
@@ -59,7 +59,7 @@ def parser_target_for_spider(target_fname):
         if is_node(at):
             current_node = get_node_name(at)
         else:
-            yield (current_node, at), [0, '']
+            yield [[current_node, at], [0, '']]
 
 class Test(unittest.TestCase):
 
@@ -73,10 +73,26 @@ class Test(unittest.TestCase):
         
     def test_parser_target(self):
         target_fname = 'test_data/test_spider_target.txt'
-        gen = parser_target_for_spider(target_fname)
-        for at in gen:
+        for at in parser_target_for_spider(target_fname):
             print at
             self.assertIsNotNone(at[0], "File exist")
+           
+        print  
+        g_for_reduce = parser_target_for_spider(target_fname)
+        
+        """
+        def f(x, y):
+            x = x[0]
+            print x
+            x.append(y[0])
+            print x
+            #yield x
+            return x
+        
+        result = reduce(f, g_for_reduce)
+        print result
+    #@for at in result:
+     #       print at"""
         
     def test_parser_target_bad_format(self):
         target_fname = 'test_data/test_spider_target_bad.txt'
