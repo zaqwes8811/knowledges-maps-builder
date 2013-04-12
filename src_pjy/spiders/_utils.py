04734e176bcecd1640449ee8a85dfe24cc29ca79
bb <bb@gmail.com>
2013-04-12 22:39:33 +0400

    Adding params...

--- src_pjy/spiders/_utils.py
+++ src_pjy/spiders/_utils.py
@@ -10,12 +10,42 @@ import re
 # Own
 import dals.os_io.io_wrapper as dal
 
+def _parse_target_params(str_params):
+    rpt = []
+    if str_params.count('[') != str_params.count(']'):
+        return None, 1, "\tError: [Count '[' != count ']']"
+
+    if str_params.count(':') != str_params.count('[') or \
+        str_params.count(':') != str_params.count(']'):
+        return None, 1, "\tError: [Format param - [anything : something]]"
+
+    if 'std' in str_params:
+        return '', 1, "Test rpt"
+    else:
+        return '', 0, None
+
+    
+def _is_node(line):
+    line = remove_forward_and_back_spaces(line)
+    if line[0] == '[' and line[-1] == ']':
+        return True
+    else:
+        return False
+    
+def get_node_name(src_node_name): 
+    return remove_forward_and_back_spaces(src_node_name.replace('[', '').replace(']', ''))
+    
+def remove_forward_and_back_spaces(line):
+    if line:
+        return re.sub("^\s+|\n|\r|\s+$", '', line)
+    else:
+        return None
+
 def parser_target_for_spider(target_fname):
     """ 
     
     Thinks:
         А что если файл пустой?
-        
         
     TODO:
         Сделать кастомизацию преобразоватлелей в текст
@@ -24,14 +54,10 @@ def parser_target_for_spider(target_fname):
     sets['name'] = target_fname
     list_lines, err = dal.efile2list(sets)
     if err[0]:
-        yield None, err
+        rpt = err[1]
+        yield None, 1, rpt
         return
-    
-    # Utils
-    remove_forward_and_back_spaces = lambda line: \
-        re.sub("^\s+|\n|\r|\s+$", '', line) if line else None
-
-    
+   
     # Можно обрабатывать
     list_without_comments = map(
             lambda line: remove_forward_and_back_spaces(line.split('#')[0]), 
@@ -40,23 +66,39 @@ def parser_target_for_spider(target_fname):
     # Удаление пустых строк
     result_job_list = []
     map(lambda line: result_job_list.append(line) if line else None, list_without_comments)
-    
-    is_node = lambda line: True if '[' in line and ']' in line else False
-    
+
     # В первой информационной строке должно быть имя узла
-    if not is_node(result_job_list[0]):
-        yield None, [2, 'Неверный формат файла - первое имя узла должно быть до адресов.']
+    if not _is_node(result_job_list[0]):
+        rpt = 'target_fname: '+target_fname+'. Неверный формат файла - первое имя узла должно быть до адресов.'
+        code_err = 2
+        yield None, code_err, rpt
         return
-    
-    get_node_name = lambda src_node_name: remove_forward_and_back_spaces(
-                                        src_node_name.replace('[', '').replace(']', ''))
     
     current_node = get_node_name(result_job_list[0])
     i = 0
+
     for at in result_job_list:
-        if is_node(at):
+        if _is_node(at):
             current_node = get_node_name(at)
             i = 0
         else:
             i += 1
-            yield (current_node, at, i)
+            # Выделяем обработчик
+            pos_first_settings_item = at.find('[')
+            if pos_first_settings_item != -1:
+                params = at[pos_first_settings_item:]
+                url =  remove_forward_and_back_spaces(
+                        at[:pos_first_settings_item])
+                params, code_err, rpt = _parse_target_params(params)
+                if code_err != 0 and rpt:
+                    rpt = 'Name node: ['+current_node+']\nUrl: ['+url+']\n\t'+rpt+''
+                yield (current_node, url, i, params), 0, rpt
+            else:
+                rpt = None
+                yield (current_node, url, i, None), 0, rpt
+                
+            
+            
+            
+            
+            
--- src_pjy/spiders/test_spider.py
+++ src_pjy/spiders/test_spider.py
@@ -20,13 +20,12 @@ import dis
 
 # 
 from spiders._utils import parser_target_for_spider
+from spiders import _utils
 
-
-
+def printer(msg):
+    print msg
 
 class Test(unittest.TestCase):
-
-
     def test_parser_target_bad_file(self):
         target_fname = 'test_spider_target.txt_f'
         gen = parser_target_for_spider(target_fname)
@@ -36,20 +35,25 @@ class Test(unittest.TestCase):
         
     def test_parser_target(self):
         target_fname = 'test_data/test_spider_target.txt'
+        parse_file_rpt = []
         for at in parser_target_for_spider(target_fname):
-            print at
-            self.assertIsNotNone(at[0], "File exist")
+            result, err_code, rpt = at
+            if rpt:
+                parse_file_rpt.append(rpt)
+            self.assertIsNotNone(result, rpt)
+        print 'Rpt:'
+        map(printer, parse_file_rpt)
            
         print  
         g_for_reduce = parser_target_for_spider(target_fname)
-        
+        #print dir(_utils)
         
     def test_parser_target_bad_format(self):
         target_fname = 'test_data/test_spider_target_bad.txt'
         gen = parser_target_for_spider(target_fname)
         for at in gen:
-            tmp, err = at
-            self.assertEqual(err[0], 2, "Ошибка форматирование файла задания")
+            tmp, err_code, rpt = at
+            self.assertEqual(err_code, 2, "Ошибка форматирование файла задания")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
