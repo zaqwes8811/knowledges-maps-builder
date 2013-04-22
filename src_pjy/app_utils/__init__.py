d2f72aa6e5c9ffd710fd9d3d70275e586ceab8c6
bb <bb@gmail.com>
2013-04-22 19:30:07 +0400

    mapreduce work

--- src_pjy/app_utils/__init__.py
+++ src_pjy/app_utils/__init__.py
@@ -2,6 +2,25 @@
 import nltk.data
 import dals
 
+# coding: utf-8
+# Std
+import re
+
+
+
+def remove_forward_and_back_spaces(line):
+    if line:
+        return re.sub("^\s+|\n|\r|\s+$", '', line)
+    else:
+        return None
+    
+def remove_fandb_spaces_in_tuple(src):
+    tmp = []
+    for at in src:
+        tmp.append(remove_forward_and_back_spaces(at))
+        
+    return tuple(tmp)
+
 if __name__=='__main__':
     print 'Done'
     
