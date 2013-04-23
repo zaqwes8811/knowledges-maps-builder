7089bd03b93ab61c0aecd41f6094c6c648961278
zaqwes <igor.a.lugansky@gmail.com>
2013-04-23 10:12:26 +0300

    Compress

--- src/TestLuceneStemmer.java
+++ src/TestLuceneStemmer.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedHashTreeMap;
 
 public class TestLuceneStemmer {
     public String stem(String fname) {
@@ -53,25 +54,53 @@ public class TestLuceneStemmer {
             String s;
             PrintWriter out = new PrintWriter("apps/out.txt");
             while ((s = in.readLine())!= null) {
-                String word = s;
-                System.out.println(s);
+                /*String word = s;
+                //System.out.println(s);
                 russianStemmer rs_new = new russianStemmer();
                 rs_new.setCurrent(word);
                 rs_new.stem();
-                String result = rs_new.getCurrent();
+                String result = rs_new.getCurrent();     */
 
                 Gson gson = new Gson();
 
 
                 //System.out.println(gson.fromJson(result, List.class));
-                HashMap<String, ArrayList<HashMap<Integer, String>>> collectionType =
-                    new HashMap<String, ArrayList<HashMap<Integer,String>>>();
+                HashMap<String, ArrayList<LinkedHashTreeMap<Integer, String>>> collectionType =
+                    new HashMap<String, ArrayList<LinkedHashTreeMap<Integer,String>>>();
                 //new Map<String, List<Map<Integer, String>>>();
-                Map<String, List<Map<Integer, String>>> type =
-                        gson.fromJson(result, collectionType.getClass());
+                HashMap<String, ArrayList<LinkedHashTreeMap<Integer, String>>>  type =
+                        gson.fromJson(s, collectionType.getClass());
 
                 // Можно обрабатывать
-                System.out.println(type);
+                //System.out.println(type);
+                //for(String str: type.keySet()) {
+                //    System.out.println(type[str]);
+                //}
+
+                for (Map.Entry<String, ArrayList<LinkedHashTreeMap<Integer, String>>> entry : type.entrySet()) {
+                    ArrayList<LinkedHashTreeMap<Integer, String>> value = entry.getValue();
+                    System.out.println(value);
+                    int len = value.size();
+                    for(int i = 0; i < len; ++i) {
+                        LinkedHashTreeMap<Integer, String> wordTuple = value.get(i);
+                        //System.out.println(wordTuple);
+                        // Слова
+                        for (Map.Entry<Integer, String> entry_word : wordTuple.entrySet()) {
+                            String wordOne = entry_word.getValue();
+
+                            String word = wordOne;
+                            //System.out.println(s);
+                            russianStemmer rs_new = new russianStemmer();
+                            rs_new.setCurrent(word);
+                            rs_new.stem();
+                            String result = rs_new.getCurrent();
+
+                            System.out.println(result);
+                        }
+                    }
+                    break;
+                    // Do things with the list
+                }
 
                 // Пишем результат
                 //out.println(word);
