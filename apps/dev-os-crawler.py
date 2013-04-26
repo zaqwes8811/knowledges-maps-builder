ba62bd67ed4256e496d7cdcf6b76c3da42d29a14
bb <bb@gmail.com>
2013-04-26 17:32:09 +0400

    doing base crawler

--- apps/dev-os-crawler.py
+++ apps/dev-os-crawler.py
@@ -6,11 +6,15 @@ Created on 26.04.2013
 '''
 
 # DAL
-import dals.local_host.io_wrapper
+import dals.local_host.io_wrapper as os_dal
 
 # Exist API
 import crawlers.dirs_walker as os_walker
 
 if __name__=='__main__':
+    target_fname = 'target/test_crawler_schedule.txt'
+    
+    def printer(msg):
+        print msg
     
     print 'Done'
