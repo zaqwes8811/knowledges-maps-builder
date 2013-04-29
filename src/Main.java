2c200d242ca9e7e0297591ad3ff3ced890107026
bb <bb@gmail.com>
2013-04-29 20:04:11 +0400

    No compiled LinkedHashTreeMap form gson, but compiled befor

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import org.yaml.snakeyaml.Yaml;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
       String path_to_cfg = "";
+
+
+      Yaml yaml = new Yaml();
+      String document = "  a: 1\n  b:\n    c: 3\n    d: 4\n";
+      System.out.println(document);
+      System.out.println(yaml.dump(yaml.load(document)));
     }
 }
