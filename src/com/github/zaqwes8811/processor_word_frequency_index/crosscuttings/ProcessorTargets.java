40275d26edb06308061dfddc408b118d7369bb3b
bb <bb@gmail.com>
2013-04-29 22:26:36 +0400

    Do parser target

--- src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
+++ src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
@@ -1,9 +1,13 @@
 package com.github.zaqwes8811.processor_word_frequency_index.crosscuttings;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,15 +17,26 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class ProcessorTargets {
+  final static int NODE_NAME = 1;
+  final static int INDEX_URL = 1;
+
   public void runParser(String targetPartPath) {
     try {
       BufferedReader java_in = new BufferedReader(new FileReader(targetPartPath+".txt"));
       //writer = ProfilingWriter();
       while (true) {
         String s = java_in.readLine();
-        print(s);
         if (s == null)
           break;
+
+        Iterable<String> result = Splitter.on('*')
+                                          .trimResults()
+                                          .omitEmptyStrings()
+                                          .split(s);
+        List<String> elements = Lists.newArrayList(result);
+        String url = elements.get(INDEX_URL);
+        print(url);
+
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
