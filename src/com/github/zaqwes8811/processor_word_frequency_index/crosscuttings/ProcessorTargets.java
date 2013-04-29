b3a7cb50842d60316d57f1885490686e81abaf1e
bb <bb@gmail.com>
2013-04-29 23:04:15 +0400

    fix

--- src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
+++ src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
@@ -1,5 +1,6 @@
 package com.github.zaqwes8811.processor_word_frequency_index.crosscuttings;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 
@@ -7,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +18,11 @@ import java.util.List;
  * Time: 21:10
  * To change this template use File | Settings | File Templates.
  */
+
+// TODO(zaqwes): Как убрать боковые пробелы из строки без сплиттера и джоинера?
+// TODO(zaqwes): Сделано очень плохо! Может для именвание узлов не испльзовать []
+//   Guava and Python can remove spaces in begin and in end
+// TODO(zaqwes): но вообще подумать над удалением заданных краевых символов строки
 public class ProcessorTargets {
   final static int NODE_NAME = 0;
   final static int INDEX_URL = 1;
@@ -23,15 +30,17 @@ public class ProcessorTargets {
   public void runParser(String targetPartPath) throws CrosscuttingsException {
     // Строка задания = [Node name]*url*...
     String  targetPartPathUrlMapper = targetPartPath+".txt";
+    List<List<String>> listUrls = new ArrayList<List<String>>();
     try {
-      BufferedReader java_in = new BufferedReader(new FileReader(targetPartPathUrlMapper));
+      BufferedReader in = new BufferedReader(new FileReader(targetPartPathUrlMapper));
 
       // Получаем строку задания
       while (true) {
-        String s = java_in.readLine();
+        String s = in.readLine();
         if (s == null)
           break;
 
+        // Дробим задания на блоки
         Iterable<String> result =
             Splitter.on('*')
                     .trimResults()
@@ -40,9 +49,15 @@ public class ProcessorTargets {
 
         // Строка задания по частям
         List<String> elements = Lists.newArrayList(result);
-        String url = elements.get(INDEX_URL);
-        print(url);
+        String nodeName = elements.get(NODE_NAME);
+
+        String purgedNodeName = extractNodeName(nodeName);
+        List<String> resultItem = elements.subList(NODE_NAME, INDEX_URL+1);
+        resultItem.set(NODE_NAME, purgedNodeName);
+
+        print(resultItem);
       }
+
     } catch (FileNotFoundException e) {
       e.printStackTrace();
       throw new CrosscuttingsException("File no found - "+targetPartPathUrlMapper);
@@ -51,11 +66,35 @@ public class ProcessorTargets {
       e.printStackTrace();
       throw new CrosscuttingsException("Error on read file - "+targetPartPathUrlMapper);
     }
+    // TODO(zaqwes) : Как быть с закрытием ресурса?
+    //finally {
+    //  in.close();
+    //}
   }
 
   private void print(Object msg) {
       System.out.println(msg);
   }
 
+  // TODO(zaqwes): impl. very bad!!
+  private String extractNodeName(String line) {
+    // Очищаем имя узла
+    Joiner joiner = Joiner.on("").skipNulls();
 
+    Iterable<String> purgeNode =
+      Splitter.on('[')
+      .trimResults()
+      .omitEmptyStrings()
+      .split(line);
+
+    String tmp = joiner.join(purgeNode);
+    purgeNode =
+      Splitter.on(']')
+        .trimResults()
+        .omitEmptyStrings()
+        .split(tmp);
+
+    //print('*'+joiner.join(purgeNode)+'*');
+    return joiner.join(purgeNode);
+  }
 }
