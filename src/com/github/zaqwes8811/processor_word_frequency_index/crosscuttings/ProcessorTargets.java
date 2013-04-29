278c949eb2b91ade9ca55ba8fb225dd0b05e2088
bb <bb@gmail.com>
2013-04-29 22:16:10 +0400

    fix

--- src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
+++ src/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargets.java
@@ -1,5 +1,10 @@
 package com.github.zaqwes8811.processor_word_frequency_index.crosscuttings;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: кей
@@ -8,6 +13,29 @@ package com.github.zaqwes8811.processor_word_frequency_index.crosscuttings;
  * To change this template use File | Settings | File Templates.
  */
 public class ProcessorTargets {
+  public void runParser(String targetPartPath) {
+    try {
+      BufferedReader java_in = new BufferedReader(new FileReader(targetPartPath+".txt"));
+      //writer = ProfilingWriter();
+      while (true) {
+        String s = java_in.readLine();
+        print(s);
+        if (s == null)
+          break;
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+
+    }
+  }
+
+  private void print(Object msg) {
+      System.out.println(msg);
+  }
 
 
 }
---
+++ tests/com/github/zaqwes8811/processor_word_frequency_index/crosscuttings/ProcessorTargetsTest.java
@@ -0,0 +1,20 @@
+package com.github.zaqwes8811.processor_word_frequency_index.crosscuttings;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 29.04.13
+ * Time: 21:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProcessorTargetsTest {
+  @Test
+  public void testRunParser() throws Exception {
+    ProcessorTargets testObj = new ProcessorTargets();
+
+    String spiderTargetFname = "apps/targets/spider_extractor_target";
+    testObj.runParser(spiderTargetFname);
+  }
+}
