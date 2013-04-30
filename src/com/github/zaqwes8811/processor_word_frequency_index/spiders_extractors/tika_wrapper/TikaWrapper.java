27e972f47025c32fa3fe3ed4d868a776b0071119
bb <bb@gmail.com>
2013-04-30 18:44:24 +0400

    fix

--- src/com/github/zaqwes8811/processor_word_frequency_index/spiders_extractors/tika_wrapper/TikaWrapper.java
+++ src/com/github/zaqwes8811/processor_word_frequency_index/spiders_extractors/tika_wrapper/TikaWrapper.java
@@ -1,13 +1,11 @@
 package com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.tika_wrapper;
 
-import java.io.File;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 
 import org.apache.tika.detect.DefaultDetector;
 import org.apache.tika.detect.Detector;
+import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -16,31 +14,42 @@ import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
 
 import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
 
 public class TikaWrapper {
   public void process(String filename) {
-    ParseContext context = new ParseContext();
-    Detector detector = new DefaultDetector();
-    Parser parser = new AutoDetectParser(detector);
-    context.set(Parser.class, parser);
-    OutputStream outputstream = new ByteArrayOutputStream();
-    Metadata metadata = new Metadata();
+    try {
+      ParseContext context = new ParseContext();
+      Detector detector = new DefaultDetector();
+      Parser parser = new AutoDetectParser(detector);
+      context.set(Parser.class, parser);
+      OutputStream outputstream = new ByteArrayOutputStream();
+      Metadata metadata = new Metadata();
 
-    URL url;
-    File file = new File(filename);
-    if (file.isFile()) {
-      url = file.toURI().toURL();
-    } else {
-      url = new URL(filename);
+      URL url;
+      File file = new File(filename);
+      if (file.isFile()) {
+        url = file.toURI().toURL();
+      } else {
+        url = new URL(filename);
+      }
+      InputStream input = TikaInputStream.get(url, metadata);
+      ContentHandler handler = new BodyContentHandler(outputstream);
+      parser.parse(input, handler, metadata, context);
+      input.close();
+
+      //Get the text into a String object
+      String extractedText = outputstream.toString();
+      //Do whatever you want with this String object.
+
+      // Пишем в файл. Формать должен быть UTF-8
+      System.out.println(extractedText);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SAXException e) {
+      e.printStackTrace();
+    } catch (TikaException e) {
+      e.printStackTrace();
     }
-    InputStream input = TikaInputStream.get(url, metadata);
-    ContentHandler handler = new BodyContentHandler(outputstream);
-    parser.parse(input, handler, metadata, context);
-    input.close();
-
-    //Get the text into a String object
-    String extractedText = outputstream.toString();
-    //Do whatever you want with this String object.
-    System.out.println(extractedText);
   }
 }
