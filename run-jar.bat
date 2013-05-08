4d9479c9a04f8205f944416c5af24c530908e5a8
bb <bb@gmail.com>
2013-05-08 18:02:42 +0400

    fix

--- run-jar.bat
+++ run-jar.bat
@@ -1,3 +1,7 @@
 java -Dfile.encoding=UTF8  -jar ^
   ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar ^
   com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderExtractor
+
+java -Dfile.encoding=UTF8  -jar ^
+  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar ^
+  com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderProcessor
