e55c7709cc098e679241ac7d8ca644e798b82fb0
bb <bb@gmail.com>
2013-05-08 18:16:14 +0400

    run from jar both files

--- run-jar.bat
+++ run-jar.bat
@@ -1,7 +1,9 @@
-java -Dfile.encoding=UTF8  -jar ^
-  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar ^
-  com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderExtractor
+rem java -Dfile.encoding=UTF8  -jar ^
+rem   ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar ^
+rem   com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderExtractor
 
-java -Dfile.encoding=UTF8  -jar ^
-  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar ^
-  com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderProcessor
+rem java -Dfile.encoding=UTF8  -jar  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderExtractor
+rem java -Dfile.encoding=UTF8  -jar  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar com.github.zaqwes8811.processor_word_frequency_index.spiders_processors.MinimalSpiderProcessor
+
+java -Dfile.encoding=UTF8  -cp  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar com.github.zaqwes8811.processor_word_frequency_index.spiders_extractors.MinimalSpiderExtractor
+java -Dfile.encoding=UTF8  -cp  ./out/artifacts/processor_word_frequency_index_jar/processor-word-frequency-index.jar com.github.zaqwes8811.processor_word_frequency_index.spiders_processors.MinimalSpiderProcessor
--- src/com/github/zaqwes8811/processor_word_frequency_index/spiders_processors/MinimalSpiderProcessor.java
+++ src/com/github/zaqwes8811/processor_word_frequency_index/spiders_processors/MinimalSpiderProcessor.java
@@ -171,7 +171,7 @@ public class MinimalSpiderProcessor {
         // записываем контетн
         BufferedWriter contentOut = writeCloser.register(new BufferedWriter(new FileWriter(path+"/content.txt")));
         contentOut.write(summaryContent.toString());
-        print(summaryContent.toString());
+        //print(summaryContent.toString());
         // записываем матеданные
         Gson gson = new Gson();
         BufferedWriter metaOut = writeCloser.register(new BufferedWriter(new FileWriter(path+"/meta.txt")));
@@ -195,8 +195,9 @@ public class MinimalSpiderProcessor {
 
     // Обрабатываем каждый узел в отдельности
     for (String node : nodes) {
+      System.console().writer().println(node);
       spiderProcessor.processOneNode(node);
-      break;  // DEVELOP
+      //break;  // DEVELOP
     }
   }
 }
