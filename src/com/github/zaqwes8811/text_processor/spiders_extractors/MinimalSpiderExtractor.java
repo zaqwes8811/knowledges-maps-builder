002f1d8a6081f34010a7a103765000f7a9308072
bb <bb@gmail.com>
2013-05-12 09:03:11 +0400

    troubles on connection web-view

--- src/com/github/zaqwes8811/text_processor/spiders_extractors/MinimalSpiderExtractor.java
+++ src/com/github/zaqwes8811/text_processor/spiders_extractors/MinimalSpiderExtractor.java
@@ -4,13 +4,17 @@ import com.github.zaqwes8811.text_processor.AppConstants;
 import com.github.zaqwes8811.text_processor.common.ImmutableAppUtils;
 import com.github.zaqwes8811.text_processor.crosscuttings.CrosscuttingsException;
 import com.github.zaqwes8811.text_processor.jobs_processors.ImmutableProcessorTargets;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 
 public class MinimalSpiderExtractor {
+  //private static Logger log = Logger.getLogger(MinimalSpiderExtractor.class);
   // Main()
   public static void main(String [ ] args) {
+    //BasicConfigurator.configure();
     try {
       // Получаем цели
       List<List<String>> targets = ImmutableProcessorTargets.runParser(AppConstants.SPIDER_TARGETS_FILENAME);
