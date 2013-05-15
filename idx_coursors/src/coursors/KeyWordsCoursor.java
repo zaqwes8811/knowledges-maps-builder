b1eb040e8ddcab89a6f0f6ec74c7b26fbaf1a3ce
bb <bb@gmail.com>
2013-05-15 19:39:03 +0400

    fix

---
+++ idx_coursors/src/coursors/KeyWordsCoursor.java
@@ -0,0 +1,27 @@
+package coursors;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 15.05.13
+ * Time: 19:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class KeyWordsCoursor {
+  public static void main(String [] args) {
+    // Need read nodes
+    List<String> nodes = ImmutableBaseCoursor.getListNodes();
+
+    // Real processing
+    for (String node: nodes) {
+
+
+      // Получаем оценки
+    }
+
+    // rpt
+    String rptFileName = "";
+  }
+}
