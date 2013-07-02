7d5d189dd57b3950f2d5f572bb4ac4f697798921
bb <bb@gmail.com>
2013-07-02 21:23:31 +0400

    NO DRY.

---
+++ src/crosscuttings/AppConfigurerTest.java
@@ -0,0 +1,17 @@
+package crosscuttings;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 29.04.13
+ * Time: 21:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppConfigurerTest {
+  @Test
+  public void testGetPathToAppFolder() throws Exception {
+
+  }
+}
