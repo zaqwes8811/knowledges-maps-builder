ed2037bdd8e9c5553866e84522c261e268895b01
bb <bb@gmail.com>
2013-07-04 15:04:15 +0400

    save

--- src/parsers/ImmutableBECParser.java
+++ src/parsers/ImmutableBECParser.java
@@ -10,54 +10,71 @@ import crosscuttings.AppConstants;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// Наверное случайный выборщик будет оборачивать это класс, а не будет встроенным в него.
+// Если его использовать статическим, то его нельзя будет передать!
 public class ImmutableBECParser {
+  public static final Optional<ImmutableBECParser> getInstance () {
+    return INSTANCE;
+  }
+
+  public static final Optional<ImmutableBECParser> INSTANCE = Optional.of(new ImmutableBECParser());
+
   private ImmutableBECParser() {}
 
   private static final Integer KEY_POS = 0;
+  private static final String SPLITTER = "@";
+  private static final String FAKE_TRANSLATE = "No";
+  private static final String PATH = "statistic-data";
+  private static final String FILENAME = "vocabularity-folded.txt";
+  private static final String FULL_FILENAME = Joiner.on(AppConstants.PATH_SPLITTER).join(PATH, FILENAME);
+
 
   private static final Optional<ImmutableList<String>> CONTENT;
+  private static final Integer COUNT_WORDS;  // Нужно будет для генератора случайных числел
+      //   но хранится будет здесь.
   // Индексты тоже сделать такими же
   private static final Optional<ImmutableList<String>> SORTED_WORDS_ALPH;
   private static final Optional<ImmutableMultimap<String, String>> WORDS_TRANSLATES;
+  private static final Optional<ImmutableMultimap<String, String>> WORDS_CONTENT;
 
   static {
+    Optional<ImmutableList<String>> content = utils.file2list(FULL_FILENAME);
 
-    // Самое тонкое место!
-    Optional<ImmutableList<String>> tmp = utils.file2list(
-      Joiner.on(AppConstants.PATH_SPLITTER)
-        .join(
-          "statistic-data",
-          "vocabularity-folded.txt"));
+    if (content.isPresent()) {
+      CONTENT = Optional.of(ImmutableList.copyOf(content.get()));
 
-    if (tmp.isPresent()) {
-      CONTENT = Optional.of(ImmutableList.copyOf(tmp.get()));
-    } else {
-      CONTENT = Optional.absent();
-    }
+      List<String> cashWords = new ArrayList<String>();
+      Multimap<String, String> cashTranslate = HashMultimap.create();
+      Multimap<String, String> cashContent = HashMultimap.create();
 
-    // Конкретные данные.
-    List<String> sortedWordsAlph = new ArrayList<String>();
-    Multimap<String, String> cashTranslate = HashMultimap.create();
-
-    if (CONTENT.isPresent()) {
       for (String record: CONTENT.get()) {
-        List<String> parsedLine = Lists.newArrayList(Splitter.on("@").split(record));
+        List<String> parsedLine = Lists.newArrayList(Splitter.on(SPLITTER).split(record));
         if (!parsedLine.isEmpty()) {
           String word = parsedLine.get(KEY_POS);
-          sortedWordsAlph.add(word);
-          cashTranslate.put(word, "No");
+          cashWords.add(word);
+          cashTranslate.put(word, FAKE_TRANSLATE);
+          cashContent.putAll(word, parsedLine.subList(KEY_POS, parsedLine.size()));
         }
       }
+
       WORDS_TRANSLATES = Optional.of(ImmutableMultimap.copyOf(cashTranslate));
-      SORTED_WORDS_ALPH = Optional.of(ImmutableList.copyOf(sortedWordsAlph));
+      SORTED_WORDS_ALPH = Optional.of(ImmutableList.copyOf(cashWords));
+      WORDS_CONTENT = Optional.of(ImmutableMultimap.copyOf(cashContent));
+      COUNT_WORDS = SORTED_WORDS_ALPH.get().size();
     } else {
+      CONTENT = Optional.absent();
       SORTED_WORDS_ALPH = Optional.absent();
       WORDS_TRANSLATES = Optional.absent();
-      utils.print("Can't read file");
+      WORDS_CONTENT = Optional.absent();
+      COUNT_WORDS = 0;
+      utils.print("Can't read file - "+FULL_FILENAME);
     }
   }
 
   public static void main(String[] args) {
+    //Optional<ImmutableBECParser> parser = ImmutableBECParser.getInstance();
+
     utils.print(WORDS_TRANSLATES);
   }
 }
