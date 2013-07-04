7c1a05933d724c63c88394385aea972fc49bf6ff
bb <bb@gmail.com>
2013-07-04 12:19:19 +0400

    save

--- src/parsers/ImmutableBECParser.java
+++ src/parsers/ImmutableBECParser.java
@@ -3,28 +3,46 @@ package parsers;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import common.utils;
 import crosscuttings.AppConstants;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImmutableBECParser {
+  private ImmutableBECParser() {}
+
+  private static final Integer KEY_POS = 0;
+
   private static final Optional<List<String>> CONTENT;
+  // Индексты тоже сделать такими же
+  private static final Optional<List<String>> SORTED_WORDS_ALPH;
+  private static final Optional<List<String>> WORDS_TRANSLATES;
+
   static {
     CONTENT = utils.file2list(
         Joiner.on(AppConstants.PATH_SPLITTER)
           .join(
               "statistic-data",
               "vocabularity-folded.txt"));
+    List<String> sortedWordsAlph = new ArrayList<String>();
+
+    if (CONTENT.isPresent()) {
+      for (String record: CONTENT.get()) {
+        List<String> parsedLine = Lists.newArrayList(Splitter.on("@").split(record));
+        if (!parsedLine.isEmpty()) {
+          sortedWordsAlph.add(parsedLine.get(KEY_POS));
+        }
+      }
+      SORTED_WORDS_ALPH = Optional.of(sortedWordsAlph);
+    } else {
+      SORTED_WORDS_ALPH = Optional.absent();
+      utils.print("Can't read file");
+    }
   }
 
   public static void main(String[] args) {
-    if (CONTENT.isPresent()) {
-      for (String record: CONTENT.get()) {
-        utils.print(Splitter.on("@").split(record));
-      }
-    } else {
-      utils.print("Can't read file");
-    }
+
   }
 }
