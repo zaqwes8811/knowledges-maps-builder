b34e8ea7a46a5650ea7c3a9fd9188e7dfbbe7e2f
bb <bb@gmail.com>
2013-07-04 15:04:15 +0400

    save

---
+++ src/words_getters/ImmutableUniformGetter.java
@@ -0,0 +1,12 @@
+package words_getters;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 04.07.13
+ * Time: 13:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImmutableUniformGetter {
+
+}
