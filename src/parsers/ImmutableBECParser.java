2fa95d89f9da1b4920315e0e0fa452e044a68686
bb <bb@gmail.com>
2013-07-04 12:48:24 +0400

    Add translate multymap.

--- src/parsers/ImmutableBECParser.java
+++ src/parsers/ImmutableBECParser.java
@@ -3,8 +3,7 @@ package parsers;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import com.google.common.collect.*;
 import common.utils;
 import crosscuttings.AppConstants;
 
@@ -19,7 +18,7 @@ public class ImmutableBECParser {
   private static final Optional<ImmutableList<String>> CONTENT;
   // Индексты тоже сделать такими же
   private static final Optional<ImmutableList<String>> SORTED_WORDS_ALPH;
-  //private static final Optional<List<String>> WORDS_TRANSLATES;
+  private static final Optional<ImmutableMultimap<String, String>> WORDS_TRANSLATES;
 
   static {
 
@@ -38,22 +37,27 @@ public class ImmutableBECParser {
 
     // Конкретные данные.
     List<String> sortedWordsAlph = new ArrayList<String>();
+    Multimap<String, String> cashTranslate = HashMultimap.create();
 
     if (CONTENT.isPresent()) {
       for (String record: CONTENT.get()) {
         List<String> parsedLine = Lists.newArrayList(Splitter.on("@").split(record));
         if (!parsedLine.isEmpty()) {
-          sortedWordsAlph.add(parsedLine.get(KEY_POS));
+          String word = parsedLine.get(KEY_POS);
+          sortedWordsAlph.add(word);
+          cashTranslate.put(word, "No");
         }
       }
+      WORDS_TRANSLATES = Optional.of(ImmutableMultimap.copyOf(cashTranslate));
       SORTED_WORDS_ALPH = Optional.of(ImmutableList.copyOf(sortedWordsAlph));
     } else {
       SORTED_WORDS_ALPH = Optional.absent();
+      WORDS_TRANSLATES = Optional.absent();
       utils.print("Can't read file");
     }
   }
 
   public static void main(String[] args) {
-
+    utils.print(WORDS_TRANSLATES);
   }
 }
