14f8779f4f0a1c9c1f5de107019200ebf8096da2
bb <bb@gmail.com>
2013-07-04 16:01:32 +0400

    Learn java.

--- src/parsers/ImmutableBECParser.java
+++ src/parsers/ImmutableBECParser.java
@@ -7,6 +7,8 @@ import com.google.common.collect.*;
 import common.utils;
 import crosscuttings.AppConstants;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,31 +16,20 @@ import java.util.List;
 // Наверное случайный выборщик будет оборачивать это класс, а не будет встроенным в него.
 // Если его использовать статическим, то его нельзя будет передать!
 public class ImmutableBECParser {
-  public static final Optional<ImmutableBECParser> getInstance () {
+  public static final ImmutableBECParser getInstance () {
     return INSTANCE;
   }
 
-  public static final Optional<ImmutableBECParser> INSTANCE = Optional.of(new ImmutableBECParser());
-
-  private ImmutableBECParser() {}
-
-  private static final Integer KEY_POS = 0;
-  private static final String SPLITTER = "@";
-  private static final String FAKE_TRANSLATE = "No";
-  private static final String PATH = "statistic-data";
-  private static final String FILENAME = "vocabularity-folded.txt";
-  private static final String FULL_FILENAME = Joiner.on(AppConstants.PATH_SPLITTER).join(PATH, FILENAME);
-
-
-  private static final Optional<ImmutableList<String>> CONTENT;
-  private static final Integer COUNT_WORDS;  // Нужно будет для генератора случайных числел
-      //   но хранится будет здесь.
-  // Индексты тоже сделать такими же
-  private static final Optional<ImmutableList<String>> SORTED_WORDS_ALPH;
-  private static final Optional<ImmutableMultimap<String, String>> WORDS_TRANSLATES;
-  private static final Optional<ImmutableMultimap<String, String>> WORDS_CONTENT;
+  // Похоже конструкторы параметры не передать. Поэтому путь жесткая константа.
+  private static final ImmutableBECParser INSTANCE;
 
   static {
+    INSTANCE = new ImmutableBECParser();
+  }
+
+  private ImmutableBECParser() {
+
+    // Доступ может быть конкурентым, если создается несколько объектов.
     Optional<ImmutableList<String>> content = utils.file2list(FULL_FILENAME);
 
     if (content.isPresent()) {
@@ -63,18 +54,35 @@ public class ImmutableBECParser {
       WORDS_CONTENT = Optional.of(ImmutableMultimap.copyOf(cashContent));
       COUNT_WORDS = SORTED_WORDS_ALPH.get().size();
     } else {
+      // Если здесь catch, то пишет что нельзя иниц. дважды, если убрать пишет что не иниц.
       CONTENT = Optional.absent();
       SORTED_WORDS_ALPH = Optional.absent();
       WORDS_TRANSLATES = Optional.absent();
       WORDS_CONTENT = Optional.absent();
       COUNT_WORDS = 0;
-      utils.print("Can't read file - "+FULL_FILENAME);
     }
   }
+
+  private final Integer KEY_POS = 0;
+  private final String SPLITTER = "@";
+  private final String FAKE_TRANSLATE = "No";
+  private final String PATH = "statistic-data";
+  private final String FILENAME = "vocabularit_y-folded.txt";
+  private final String FULL_FILENAME = Joiner.on(AppConstants.PATH_SPLITTER).join(PATH, FILENAME);
+
+  // Допустим испльзуем
+  // Если коллекция пустая, то возможно было пустой файл, а так сразу ясно, что что-то не так
+  //   но появляние ошибки растянуто в времени.
+  private final Optional<ImmutableList<String>> CONTENT;
+  private final Integer COUNT_WORDS;  // Нужно будет для генератора случайных числел
+      //   но хранится будет здесь.
+  // Индексты тоже сделать такими же
+  private final Optional<ImmutableList<String>> SORTED_WORDS_ALPH;
+  private final Optional<ImmutableMultimap<String, String>> WORDS_TRANSLATES;
+  private final Optional<ImmutableMultimap<String, String>> WORDS_CONTENT;
 
   public static void main(String[] args) {
     //Optional<ImmutableBECParser> parser = ImmutableBECParser.getInstance();
 
-    utils.print(WORDS_TRANSLATES);
   }
 }
