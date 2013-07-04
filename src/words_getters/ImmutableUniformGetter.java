739e9ea8a707d58633bccd5bf520950ca20ece06
bb <bb@gmail.com>
2013-07-04 17:34:33 +0400

    Begin doing probablity accessors.

--- src/words_getters/ImmutableUniformGetter.java
+++ src/words_getters/ImmutableUniformGetter.java
@@ -1,12 +1,18 @@
 package words_getters;
 
+import com.google.common.base.Optional;
+import parsers.ImmutableBECParser;
+
 /**
- * Created with IntelliJ IDEA.
- * User: кей
- * Date: 04.07.13
- * Time: 13:55
- * To change this template use File | Settings | File Templates.
  */
 public class ImmutableUniformGetter {
+  public ImmutableUniformGetter(Optional<ImmutableBECParser> parser) {
+    BEC_PARSER = parser;
+  }
 
+  private final Optional<ImmutableBECParser> BEC_PARSER;
+
+  public static void main(String [] args) {
+
+  }
 }
