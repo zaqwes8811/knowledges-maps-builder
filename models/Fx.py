f5983f1404549192735d83a2835f956f79f9943e
bb <bb@gmail.com>
2013-07-10 17:13:07 +0400

    save

--- models/Fx.py
+++ models/Fx.py
@@ -2,11 +2,23 @@
 
 
 from numpy import arange
+from pylab import plot
+from pylab import show
 
 
 
 COUNT_POINTS = 23;
 fx = arange(COUNT_POINTS)
 
+Fxi = 0
+Fx = []
 for i in fx:
-    print i
+    Fx.append(Fxi)
+    Fxi += i
+    
+for j in range(4):
+    Fx.append(Fxi)
+    
+plot(Fx, "-v")
+show()
+
