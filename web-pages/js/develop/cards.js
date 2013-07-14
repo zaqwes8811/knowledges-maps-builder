3c4d85054be06ffa7ed12e4825281133ceee4ca9
bb <bb@gmail.com>
2013-07-14 19:30:49 +0400

    Try show Kat.

--- web-pages/js/develop/cards.js
+++ web-pages/js/develop/cards.js
@@ -12,21 +12,23 @@ function init() {
         var items = ['word', 'content', 'translate'];
         var TOTAL_CARDS = items.length;
         var part_name = ".pack-card-container > div.active-card";
-        $(UP_TUNER).bind(
-            { click:
-                (function(seed) {
-                    return function() {
-                        // Slot
-                        var ptr = 0;
-                        var tmp = $(this).parent().find(part_name).each(function () {
-                            // Порадок важен
-                            ++ptr; $(this).css("z-index", (seed-ptr+TOTAL_CARDS)%TOTAL_CARDS);});
-                        seed = (seed+1)%TOTAL_CARDS;
-                    }
-                })(seed)});
+        var binder = { click:
+         (function(seed) {
+             return function() {
+                 // Slot
+                 var ptr = 0;
+                 var tmp = $(this).parent().find(part_name).each(function () {
+                     // Порадок важен
+                     ++ptr; $(this).css("z-index", (seed-ptr+TOTAL_CARDS)%TOTAL_CARDS);});
+                 seed = (seed+1)%TOTAL_CARDS;
+             }
+         })(seed)};
+
+        $(UP_TUNER).bind(binder);
         // Заполняем контент
         // Удаляем старый если есть
 
+        (function() {
         // Добавляем новый
         // TODO(zaqwes): Кажется нужна таки обертка для отделения от тюнеров
         var count_content_items = content_items.length;
@@ -38,22 +40,29 @@ function init() {
             $("<span/>").addClass("text-contents").appendTo(tmp).append(content_items[i])
          }
 
+
+        // Только если более одного элемента.
         if (count_content_items > 1) {
             var seed = 0;
             // Тюнер вверх
-            $("<div/>").addClass("tuner-base tuner-base-up-inner").appendTo(full_path_to_content_card).bind(
-                { click: (function(seed) { return function() {
-                    // Slot
-                    var ptr = 0;
-                    $(this).parent().find('.pack-card-container-inner').each(function () {
-                        // Порадок важен
-                        ++ptr; $(this).css("z-index", (seed-ptr+count_content_items)%count_content_items);});
-                    seed = (seed+1)%count_content_items;
-                }})(seed)});
+            var needed_class = '.pack-card-container-inner';
+            var bind_obj = { click:
+              (function(seed, items) {
+                 return (function() {
+                   // Slot
+                   var ptr = 0;
+                   $(this).parent().find(needed_class).each(
+                     function() { ++ptr; $(this).css("z-index", (seed-ptr+items)%items); });
+                   seed = (seed+1)%items;
+                 })
+              })(seed, count_content_items)};
+            $("<div/>").addClass("tuner-base tuner-base-up-inner").appendTo(full_path_to_content_card).bind(bind_obj);
 
             // Тюнер вниз
             $("<div/>").addClass("tuner-base tuner-base-down-inner").appendTo(full_path_to_content_card);
         }
+
+        })();
 
     })(content_items);  // Вызов как бы конструктора.
 }
