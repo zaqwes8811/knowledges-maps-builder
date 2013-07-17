88d01483d2d35f66d0eec43ed5ba4c11209beded
bb <bb@gmail.com>
2013-07-17 20:53:59 +0400

    V1 Done

--- web-pages/app/cards.js
+++ web-pages/app/cards.js
@@ -14,6 +14,8 @@ var AConstants = (function () {
   return {
     ITEMS : items,   // Какие есть подкарты
     CONTENT : items[1],
+    TRANSLATE : items[2],
+    WORD : items[0],
     TOTAL_FRONT_CARDS : items.length,
 
     // Selectors
@@ -34,20 +36,29 @@ function tick(seed, sel, count, obj) {
        //obj = null;
     }
 
+function getFakeResponse() {
+    // Данные которые будут приходить с веб-сервера
+    var response = []
+    var CONTENT = AConstants.CONTENT;
+    var TRANSLATE = AConstants.TRANSLATE;
+    response.push({
+       "content" : ["Hello Display the matched.", "Display the matched elements with a sliding motion."],
+       "translate" : ["Перевод1"],
+       'word': ['matched']
+    });
+
+    response.push({
+       'content' : ["Hello"],
+       'translate' : ["Перевод", "Переводasdf"],
+       'word': ['hello']
+    });
+    return response;
+}
 //var Cards = (function)
 function init() {
-  // Данные которые будут приходить с веб-сервера
-  var response = []
-  var dataOneCard = {};
-  dataOneCard[AConstants.CONTENT] = ["Hello Display the matched.", "Display the matched elements with a sliding motion."];
-  response.push(dataOneCard);
-  var dataTwoCard = {};
-  dataTwoCard[AConstants.CONTENT] = ["Hello"];//, "Display."];
-  response.push(dataTwoCard);
-
-  processResponse(response);
-
-  pureInit()
+    var response = getFakeResponse();
+    processResponse(response);
+    pureInit()
 }
 
 function pureInit() {
@@ -76,45 +87,57 @@ function pureInit() {
 
 function processResponse(response) {
     // TODO(zaqwes): TOTH: Что будет менятся от запроса к запросу?
-
     // Обрабатываем все доступные карты
     $(AConstants.CARD_CONTAINER_SEL).each(function(key, value) {
-
         // Создаем подкарты. Так проще будет их подключить, т.к. будут дескрипторы.
         var subCardsPkg = $(this).find(AConstants.PACK_CARDS_SEL);
         subCardsPkg.empty();  // TODO(zaqwes): Не утекают ли обработчики тюнеров?
+        // http://stackoverflow.com/questions/11726864/jquery-empty-click-and-memory-management
         var cardsHandles = {};
 
         $.each((goog.array.clone(AConstants.ITEMS)).reverse(), function (key, value) {
           cardsHandles[value] = $("<div/>").addClass('active-card').appendTo(subCardsPkg);
-          $(cardsHandles[value]).css("z-index", key)
+          $(cardsHandles[value]).css("z-index", key);
         });
 
         // Добавляем контент
-        var handler = cardsHandles[AConstants.CONTENT];
-        var content = response[key][AConstants.CONTENT];
-        var countItems = content.length;
-        for (var i= 0; i < countItems; ++i) {
-          // Текст нужно обернуть получше
-          $("<span/>").addClass("text-contents").append(content[i]).appendTo(
-            $("<div/>").addClass("content-stack").appendTo(
-              $("<div/>").addClass('pack-card-container-inner').appendTo(handler)));
-         }
+        var tmp = [AConstants.CONTENT, AConstants.TRANSLATE, AConstants.WORD];
+        $.each(tmp, function(key_local, value) {
+            var handler = cardsHandles[value];
+            var content = response[key][value];
+            fillNoWordCard(content, handler);
+        });
 
-        // Добавляем тюнеры только если более одного элемента.
-        if (countItems > 1) {
-            var seed = 0;
-            $("<div/>").addClass("tuner-base tuner-base-up-inner")
-               .click(function() {
-                     seed = (seed+1)%countItems;
-                     tick(seed, AConstants.INNER_CARDS_CONTAINER, countItems, this);})
-                .appendTo(handler);
-            $("<div/>").addClass("tuner-base tuner-base-down-inner")
-                .click(function() {
-                  seed = (seed-1+countItems)%countItems;
-                  tick(seed, AConstants.INNER_CARDS_CONTAINER, countItems, this);})
-                .appendTo(handler);
-        }
-        handler = null;
+        $.each(function (key, value) { value = null; });
     });  // .each()
+}
+
+function fillNoWordCard(content, handler) {
+    var countItems = content.length;
+
+    // TODO(zaqwes): Low perfomance! Можно в цикле развернуть.
+    var contentReversed = (goog.array.clone(content)).reverse();
+    for (var i = 0; i < countItems; ++i) {
+        // Текст нужно обернуть получше
+        $("<span/>").addClass("text-contents").append(contentReversed[i]).appendTo(
+         $("<div/>").addClass("content-stack").appendTo(
+           $("<div/>").addClass('pack-card-container-inner').appendTo(handler)));
+    }
+
+    // Добавляем тюнеры только если более одного элемента.
+    // TODO(zaqwes): Сделать их по середине поля! И тонкими
+    if (countItems > 1) {
+        var seed = 0;
+        $("<div/>").addClass("tuner-base tuner-base-up-inner")
+            .click(function() {
+                seed = (seed+1)%countItems;
+                tick(seed, AConstants.INNER_CARDS_CONTAINER, countItems, this);})
+            .appendTo(handler);
+        $("<div/>").addClass("tuner-base tuner-base-down-inner")
+            .click(function() {
+                seed = (seed-1+countItems)%countItems;
+                tick(seed, AConstants.INNER_CARDS_CONTAINER, countItems, this);})
+            .appendTo(handler);
+    }
+    handler = null;  // TODO(zaqwes): TOTH: Нужно ль?
 }
