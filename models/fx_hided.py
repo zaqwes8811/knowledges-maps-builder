6b025e076f02f40d5a716f27d5302b9dbdba934b
bb <bb@gmail.com>
2013-07-19 13:35:26 +0400

    Run again.

--- models/fx_hided.py
+++ models/fx_hided.py
@@ -3,7 +3,40 @@ Created on 19.07.2013
 
 @author: кей
 '''
+class Range(object):
+    def contains(self, c):
+        pass
+    
+class ClosedOpen(Range):
+    _a = None
+    _b = None
+    _id = None
+    def __init__(self, a, b, id):
+        self._a = a
+        self._b = b
+        self._id = id
+    def get_id(self):
+        return self._id
+    
+    def get_a(self):
+        return self._a
+    
+    def get_b(self):
+        return self._b
+        
+    def contains(self, c):
+        return self._a < c and c <= self._b
+    
+    def __str__(self):
+        return "("+str(self._a)+", "+str(self._b)+"]"
+    def __repr__(self):
+        return self.__str__()
 
+def find(ranges, n, value):
+    if n != 1:
+        return ranges[0].get_a() < value and value <= ranges[n-1].get_b()  
+    else:
+        return ranges[0].contains(value)
 
 def get_near_uniform_iterative(code_book, size_code_book, max_value, ranges=None):
     """ Precond: Массив отсортирован и элементы уникальны по id. """
