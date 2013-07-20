35a5ec5c31fca564dfa9579b8ad7df058aabb2ba
bb <igor.a.lugansky@gmail.com>
2013-07-20 18:21:04 +0400

    Thinking about exception in ctr.

---
+++ src/idx_coursors/NodeAlreadyExist.java
@@ -0,0 +1,11 @@
+package idx_coursors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 20.07.13
+ * Time: 17:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class NodeAlreadyExist extends Exception {
+}
