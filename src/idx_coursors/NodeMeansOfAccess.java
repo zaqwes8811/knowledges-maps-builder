985e9e03690bdec9149c08a50d5ebff0d4b655a0
bb <igor.a.lugansky@gmail.com>
2013-07-21 16:38:38 +0400

    Thinking and ref.

--- src/idx_coursors/NodeMeansOfAccess.java
+++ src/idx_coursors/NodeMeansOfAccess.java
@@ -1,11 +1,4 @@
 package idx_coursors;
 
-/**
- * Created with IntelliJ IDEA.
- * User: кей
- * Date: 21.07.13
- * Time: 12:50
- * To change this template use File | Settings | File Templates.
- */
-public interface NodeMeansOfAccess {
-}
+// Добавляет методы записи
+public interface NodeMeansOfAccess extends ImmutableNodeMeansOfAccess {}
