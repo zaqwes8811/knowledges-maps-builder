6d335dc5b0dc0cb4810c5f5b981a2d34d5199492
bb <igor.a.lugansky@gmail.com>
2013-07-21 12:59:08 +0400

    ref = logic and phy

---
+++ src/idx_coursors/ImmutableNodeMeansOfAccess.java
@@ -0,0 +1,11 @@
+package idx_coursors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 21.07.13
+ * Time: 12:51
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ImmutableNodeMeansOfAccess {
+}
