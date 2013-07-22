bc0e7cf43a1004f14a22fcb2c4f994b30b8aab76
bb <igor.a.lugansky@gmail.com>
2013-07-22 15:50:31 +0400

    Add app_folder.

--- src/idx_coursors/NodeAlreadyExist.java
+++ src/idx_coursors/NodeAlreadyExist.java
@@ -1,11 +1,4 @@
 package idx_coursors;
 
-/**
- * Created with IntelliJ IDEA.
- * User: кей
- * Date: 20.07.13
- * Time: 17:15
- * To change this template use File | Settings | File Templates.
- */
-public class NodeAlreadyExist extends Exception {
-}
+// Вероятно ошибка пользователя. Входные данные - checked
+public class NodeAlreadyExist extends Exception {}
