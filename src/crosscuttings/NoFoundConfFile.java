fb0978f9dda5ae44e9dbadc64324f5a88a083b3d
bb <igor.a.lugansky@gmail.com>
2013-07-23 15:27:04 +0400

    Work with app configurator.

---
+++ src/crosscuttings/NoFoundConfFile.java
@@ -0,0 +1,22 @@
+package crosscuttings;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: кей
+ * Date: 23.07.13
+ * Time: 14:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoFoundConfFile extends Exception {
+  public NoFoundConfFile(String msg) {
+    super(msg);
+  }
+  public NoFoundConfFile(Exception e) {
+    super(e);
+  }
+  private String fileName;
+  public void setFileName(String name) {
+    fileName = name;
+  }
+  public String getFileName() {return fileName;}
+}
