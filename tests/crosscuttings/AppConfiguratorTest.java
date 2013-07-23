d3710b14936db256929e98a7bc413f1ed7763594
bb <igor.a.lugansky@gmail.com>
2013-07-23 19:34:58 +0400

    save

--- tests/crosscuttings/AppConfiguratorTest.java
+++ tests/crosscuttings/AppConfiguratorTest.java
@@ -4,13 +4,7 @@ import com.google.common.collect.ImmutableSet;
 import common.Util;
 import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: кей
- * Date: 23.07.13
- * Time: 14:53
- * To change this template use File | Settings | File Templates.
- */
+//
 public class AppConfiguratorTest {
   @Test//(expected = ConfFileIsCorrupted.class)
   public void testGetPathToAppFolder() throws Exception {
