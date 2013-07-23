bdd2496b9756de97f207dd7846c61bf086f6cac9
bb <igor.a.lugansky@gmail.com>
2013-07-23 18:44:18 +0400

    Folde cfg reader.

--- src/crosscuttings/AppConfigurator.java
+++ src/crosscuttings/AppConfigurator.java
@@ -23,57 +23,50 @@ public class AppConfigurator<V> {
 
   public static abstract class Reader<V> {
     // Конечную точки заворачиваем в Optional
-    public abstract Optional<V> read(String key) throws IOException;
+    public abstract Optional<V> extract(ImmutableList<String> pieces, Map object);
   }
 
   public static class ReaderDecorator<V> {
     public Optional<V> read(Reader<V> reader, String key)
-      throws NoFoundConfFile, ConfFileIsCorrupted, RecordNoFound {
-    try {
-      return reader.read(key);
-    } catch (FileNotFoundException e) {
-      NoFoundConfFile c = new NoFoundConfFile(e);
-      c.setFileName(APP_CFG_FULL_FILENAME);
-      throw c;
-    } catch (ScannerException e) {
-      throw new ConfFileIsCorrupted(e);
-    } catch (NullPointerException e) {
-      throw new RecordNoFound(e, key);
-    } catch (IOException e) {
-      throw new CrosscuttingsException(e);
+        throws NoFoundConfFile, ConfFileIsCorrupted, RecordNoFound {
+      try {
+        Closer closer = Closer.create();
+        Optional<V> path = Optional.absent();
+        try {
+          InputStream input = closer.register(new FileInputStream(new File(APP_CFG_FULL_FILENAME)));
+          Map object = (Map)(Map<String, Object>) (new Yaml()).load(input);
+          ImmutableList<String> pieces = ImmutableList.copyOf(Splitter.on("/").split(key));
+          path = reader.extract(pieces, object);
+        } catch (Throwable e) {
+          throw closer.rethrow(e);
+        } finally {
+          closer.close();
+        }
+        return path;
+      } catch (FileNotFoundException e) {
+        NoFoundConfFile c = new NoFoundConfFile(e);
+        c.setFileName(APP_CFG_FULL_FILENAME);
+        throw c;
+      } catch (ScannerException e) {
+        throw new ConfFileIsCorrupted(e);
+      } catch (NullPointerException e) {
+        throw new RecordNoFound(e, key);
+      } catch (IOException e) {
+        throw new CrosscuttingsException(e);
+      }
     }
   }
-
 
   // Папка не обязательно в рабочей директории программы
   public static Optional<String> getPathToAppFolder()
       throws NoFoundConfFile, ConfFileIsCorrupted, RecordNoFound {
-
     String requestedPath = "App/Scriber/app_folder";
     return new ReaderDecorator<String>().read(new Reader<String>() {
         @Override
-        public Optional<String> read(String key) throws IOException {
-          Closer closer = Closer.create();
-          String path;
-          try {
-            InputStream input = closer.register(new FileInputStream(new File(APP_CFG_FULL_FILENAME)));
-
-            // Обрабатываем путь
-            Map topCfg = (Map)(Map<String, Object>) (new Yaml()).load(input);
-
-            ImmutableList<String> pieces = ImmutableList.copyOf(Splitter.on("/").split(key));
-            Map step = (Map)topCfg.get(pieces.get(0));
+        public Optional<String> extract(ImmutableList<String> pieces, Map object) {
+            Map step = (Map)object.get(pieces.get(0));
             step = (Map)step.get(pieces.get(1));
-
-            // Конечную точки заворачиваем в Optional
-            path = (String)step.get(pieces.get(2));
-          } catch (Throwable e) {
-            throw closer.rethrow(e);
-          } finally {
-            closer.close();
-          }
-          return Optional.of(path);
-          //
+            return Optional.of((String)step.get(pieces.get(2)));
         }
       }, requestedPath);
   }
@@ -84,26 +77,30 @@ public class AppConfigurator<V> {
   public static Optional<ImmutableSet<String>> getRegisteredNodes()
     throws NoFoundConfFile, ConfFileIsCorrupted, RecordNoFound {
     String requestedPath = "App/registered nodes";
-    ImmutableList<String> pieces = ImmutableList.copyOf(Splitter.on("/").split(requestedPath));
-    Integer size = pieces.size();
 
-    Yaml yaml = new Yaml();
     try {
+
+      //
+      Optional<ImmutableSet<String>> result = Optional.absent();
       Closer closer = Closer.create();
       try {
         InputStream input = closer.register(new FileInputStream(new File(APP_CFG_FULL_FILENAME)));
 
         // Обрабатываем путь
+        Yaml yaml = new Yaml();
         Map topCfg = (Map)(Map<String, Object>)yaml.load(input);
 
+        ImmutableList<String> pieces = ImmutableList.copyOf(Splitter.on("/").split(requestedPath));
         Map step = (Map)topCfg.get(pieces.get(0));
         List<String> nodes = (List)step.get(pieces.get(1));
-        return Optional.of(ImmutableSet.copyOf(nodes));
+        result = Optional.of(ImmutableSet.copyOf(nodes));
       } catch (Throwable e) {
         throw closer.rethrow(e);
       } finally {
         closer.close();
       }
+      return result;
+
     } catch (FileNotFoundException e) {
       NoFoundConfFile c = new NoFoundConfFile(e);
       c.setFileName(APP_CFG_FULL_FILENAME);
