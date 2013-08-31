4daeec4d41d362c01432f3ca8f507b42219f239f
bb <igor.a.lugansky@gmail.com>
2013-08-31 18:37:03 +0400

    fix

--- tests/web_wrapper/AppContainerTest.java
+++ tests/web_wrapper/AppContainerTest.java
@@ -1,8 +1,19 @@
 package web_wrapper;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import common.Util;
+import idx_coursors.IdxNodeAccessor;
+import idx_coursors.ImmutableNodeMeansOfAccess;
+import idx_coursors.NodeIsCorrupted;
+import idx_coursors.NodeNoFound;
 import net.jcip.annotations.Immutable;
 import org.junit.Test;
+import through_functional.configurator.AppConfigurator;
+import through_functional.configurator.ConfFileIsCorrupted;
+import through_functional.configurator.NoFoundConfFile;
+import ui.UI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,20 +26,35 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class AppContainerTest {
-  @Test
-  public void testCloseApp() throws Exception {
-    /*List<Integer> result = new ArrayList<Integer>();
-    Integer SIZE_EXPERIMENT = 10000;
-    for (int i = 0; i < SIZE_EXPERIMENT; ++i) {
-      result.add(AppContainer.getKey());
+  private ImmutableList<ImmutableNodeMeansOfAccess> getNodes(ImmutableSet<String> nameNodes)
+      throws ConfFileIsCorrupted, NoFoundConfFile {
+
+    List<ImmutableNodeMeansOfAccess> tmpNodes = new ArrayList<ImmutableNodeMeansOfAccess>();
+    for (String node: nameNodes) {
+      // Загружает данные узла
+      try {
+        tmpNodes.add(IdxNodeAccessor.createImmutableConnection(node));
+      } catch (NodeIsCorrupted e) {
+        UI.showMessage("Node is corrupted - " + node);
+      } catch (NodeNoFound e) {
+        UI.showMessage("Node no found - "+node);
+      }
     }
-    Util.print(result);*/
+    ImmutableList<ImmutableNodeMeansOfAccess> nodes = ImmutableList.copyOf(tmpNodes);
+    return nodes;
   }
 
   @Test
-  public void testMain() throws Exception {
-    for (int i = 0; i < 19; ++i) {
-      Util.print(AppContainer.getPackage());
+  public void testGeneratePackage() throws Exception {
+    try {
+      ImmutableSet<String> namesNodes = AppConfigurator.getRegisteredNodes().get();
+      new AppContainer(getNodes(namesNodes)).getPackage();
+    } catch (NoFoundConfFile e) {
+      UI.showMessage("Configuration file no found - "+e.getFileName());
+      //closeApp();
+    } catch (ConfFileIsCorrupted e) {
+      UI.showMessage(e.WHAT_HAPPENED);
+      //closeApp();
     }
   }
 }
