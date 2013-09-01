ac5f781fae6237eaf37870b2877293809c91de52
bb <igor.a.lugansky@gmail.com>
2013-09-01 16:28:30 +0400

    Make content accessor.

--- tests/idx_coursors/BaseContentHolderTest.java
+++ tests/idx_coursors/BaseContentHolderTest.java
@@ -1,10 +1,58 @@
 package idx_coursors;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+class InMemorySentencesAccessor implements ContentStorageAccessor {
+  private final ImmutableList<String> CONTENT;
+  public InMemorySentencesAccessor(ImmutableList<String> content) {
+    CONTENT = content;
+  }
+
+  @Override
+  public String getSentence(Integer key) {
+    return "Sentence";
+  }
+}
+
+class ExtractSentenceException extends RuntimeException {
+  public ExtractSentenceException(Throwable e) {super(e);}
+}
+
+class CashedContentAccessor implements ContentStorageAccessor {
+  private String getSentenceFromFile(Integer key) {
+    return "Sent";
+  }
+
+  public CashedContentAccessor () {
+    GRAPHS = CacheBuilder.newBuilder()
+      .maximumSize(1000)
+      .build(
+        new CacheLoader<Integer, String>() {
+          @Override
+          public String load(Integer key) {
+            return getSentenceFromFile(key);
+          }
+        });
+  }
+  @Override
+  public String getSentence(Integer key) {
+    try {
+      return GRAPHS.get(key);
+    } catch (ExecutionException e) {
+      throw new ExtractSentenceException(e.getCause());
+    }
+  }
+
+  private final LoadingCache<Integer, String> GRAPHS;
+}
 
 public class BaseContentHolderTest {
   @Test
@@ -16,6 +64,8 @@ public class BaseContentHolderTest {
     // Коллекции Guava хранят порядок, но для сипска рекомендуюется использовать
     //   JDK версию данной коллекции.
     ImmutableList<String> sentences = ImmutableList.of("hello hay", "hay");
+
+    // Плохо передавать предложения, лучше передать кэш. Предложений может быть много
 
     ContentHolder contentHolder = new BaseContentHolder(keys, sentences);
   }
