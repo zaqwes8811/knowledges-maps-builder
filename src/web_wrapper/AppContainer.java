568a08173339069f592413e79d8003d7cd5aaec3
bb <igor.a.lugansky@gmail.com>
2013-09-15 09:39:48 +0400

    reorg

--- src/web_wrapper/AppContainer.java
+++ src/web_wrapper/AppContainer.java
@@ -2,52 +2,25 @@ package web_wrapper;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.google.gson.Gson;
 import common.math.GeneratorAnyRandom;
 import idx_coursors.*;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import through_functional.configurator.AppConfigurator;
 import through_functional.configurator.ConfFileIsCorrupted;
 import through_functional.configurator.NoFoundConfFile;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.*;
 
 
 public class AppContainer {
-
   private final ImmutableList<ImmutableNodeAccessor> NODES;
   private final ImmutableNodeAccessor ACTIVE_NODE_ACCESSOR;
   private final GeneratorAnyRandom GENERATOR;
-
-  static public ImmutableList<ImmutableNodeAccessor> getNodes(
-    ImmutableSet<String> nameNodes, IFabricImmutableNodeAccessors fabric)
-    throws ConfFileIsCorrupted, NoFoundConfFile {
-    Map<String, String> report = new HashMap<String, String>();
-    List<ImmutableNodeAccessor> accessors = new ArrayList<ImmutableNodeAccessor>();
-    for (String node: nameNodes) {
-      try {
-        ImmutableNodeAccessor accessor = fabric.create(node);
-        accessors.add(accessor);
-      } catch (NodeIsCorrupted e) {
-        report.put(node, "Is corrupted");
-      } catch (NodeNoFound e) {
-        report.put(node, "No found");
-      }
-    }
-    return ImmutableList.copyOf(accessors);
-  }
 
   // Index: нужно для маркеровки
   // Word: само слово
@@ -90,81 +63,6 @@ public class AppContainer {
     // Коннектим генератор случайных чисел и акссессор
     List<Integer> distribution = ACTIVE_NODE_ACCESSOR.getDistribution();
     GENERATOR = GeneratorAnyRandom.create(distribution);
-  }
-
-  private static Server createServer() {
-    Server server = new Server();
-
-    SelectChannelConnector connector = new SelectChannelConnector();
-    connector.setPort(8080);
-    server.addConnector(connector);
-
-    // Подключаем корень?
-    ResourceHandler resourceHandler = new ResourceHandler();
-    resourceHandler.setDirectoriesListed(true);
-    resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
-    resourceHandler.setResourceBase("./web-pages");
-
-
-    // Список обработчиков?
-    HandlerList handlers = new HandlerList();
-    ServletHandler handler = new ServletHandler();
-    handler.addServletWithMapping("web_wrapper.AppContainer$Pkg", "/pkg");
-    handlers.setHandlers(new Handler[] { resourceHandler, handler });
-    // ! если не находи index.html Открывает вид папки!!
-
-    // Подключаем к серверу
-    server.setHandler(handlers);
-    return server;
-
-
-  }
-
-  private static Server build() {
-    Server server = new Server();
-
-    SelectChannelConnector connector = new SelectChannelConnector();
-    connector.setPort(8080);
-    server.addConnector(connector);
-
-    // Подключаем ресурсы
-    ResourceHandler resourceHandler = new ResourceHandler();
-    resourceHandler.setDirectoriesListed(true);
-    resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
-    resourceHandler.setResourceBase("./web-pages");
-
-    // Регистрируем обработчики
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-
-    // Сервлеты
-    try {
-      ImmutableSet<String> namesNodes = AppConfigurator.getRegisteredNodes().get();
-      ImmutableList<ImmutableNodeAccessor> accessors = getNodes(
-          namesNodes, new FabricImmutableNodeAccessors());
-
-      //
-      AppContainer container = new AppContainer(accessors);
-      context.addServlet(new ServletHolder(new Pkg(container)),"/pkg");
-
-      // Коннектим все
-      HandlerList handlers = new HandlerList();
-      handlers.setHandlers(new Handler[] { resourceHandler, context });
-      server.setHandler(handlers);
-
-    } catch (NoFoundConfFile e) {
-      throw new RuntimeException();
-    } catch (ConfFileIsCorrupted e) {
-      throw new RuntimeException();
-    }
-
-    return server;
-  }
-
-  public static void main(String[] args) throws Exception {
-    Server server = build();
-    server.start();
-    server.join();
   }
 }
 
--- src/web_wrapper/WebRelay.java
+++ src/web_wrapper/WebRelay.java
@@ -2,43 +2,43 @@ package web_wrapper;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import idx_coursors.*;
-import org.junit.Test;
+import idx_coursors.FabricImmutableNodeAccessors;
+import idx_coursors.ImmutableNodeAccessor;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
 import through_functional.configurator.AppConfigurator;
 import through_functional.configurator.ConfFileIsCorrupted;
 import through_functional.configurator.NoFoundConfFile;
-import ui.UI;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
  * User: кей
- * Date: 25.07.13
- * Time: 19:08
+ * Date: 15.09.13
+ * Time: 9:35
  * To change this template use File | Settings | File Templates.
  */
-public class AppContainerTest {
-
-  //TODO(zaqwes): Венуть tuple - результат и отчет - если отчет пуст - все хорошо
-
-
-  @Test
-  public void testGeneratePackage() throws Exception {
+public class WebRelay {
+  public static void main(String[] args) throws Exception {
+    // Сервлеты
     try {
       Wrapper wrapper = new Wrapper();
+
       ImmutableSet<String> namesNodes = AppConfigurator.getRegisteredNodes().get();
       ImmutableList<ImmutableNodeAccessor> accessors = wrapper.getNodes(
         namesNodes, new FabricImmutableNodeAccessors());
+
       AppContainer container = new AppContainer(accessors);
-      container.getPackageActiveNode();
+
+      HandlerList handlers = Wrapper.buildHandlers(container);
+
+      Server server = new Server(8080);
+      server.setHandler(handlers);
+      server.start();
+      server.join();
     } catch (NoFoundConfFile e) {
-      UI.showMessage("Configuration file no found - "+e.getFileName());
+      throw new RuntimeException();
     } catch (ConfFileIsCorrupted e) {
-      UI.showMessage(e.WHAT_HAPPENED);
+      throw new RuntimeException();
     }
   }
 }
