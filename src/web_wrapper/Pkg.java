9fdd59e5cf0251a3b82101c50a78161ee73a4cf7
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-11-30 18:37:34 +0400

    Remove public constructor for servlet Pkg

--- src/web_wrapper/Pkg.java
+++ src/web_wrapper/Pkg.java
@@ -1,25 +1,44 @@
 package web_wrapper;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
+
+import idx_coursors.FabricImmutableNodeAccessors;
+import idx_coursors.ImmutableNodeAccessor;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import through_functional.configurator.AppConfigurator;
+import ui.UI;
+
 import java.io.IOException;
 
 class Pkg extends HttpServlet {
-  private final Concentrator CONTAINER;
-  public Pkg(Concentrator container) {
-    CONTAINER = container;
-  }
-
-  // Используем идею REST - параметры GET запросе не передаются
+  private Concentrator CONTAINER;
   @Override
-  protected void doGet(
-    HttpServletRequest request,
-    HttpServletResponse response) throws ServletException, IOException
-  {
+  public void doGet(
+  		HttpServletRequest request,
+  		HttpServletResponse response) throws ServletException, IOException
+  	{
+  	
+  	try {
+  	if(null != CONTAINER) {
+  		Wrapper wrapper = new Wrapper();
+
+      String pathToCfgFile = "app.yaml";
+      ImmutableSet<String> namesNodes = new AppConfigurator(pathToCfgFile).getRegisteredNodes().get();
+      ImmutableList<ImmutableNodeAccessor> accessors = wrapper.getNodes(
+          namesNodes, new FabricImmutableNodeAccessors());
+
+      CONTAINER = new AppConcentrator(accessors);
+  	}
+  	} catch (Exception e) {
+  		UI.showMessage(e);
+  	}
 
     response.setContentType("text/html");
     response.setStatus(HttpServletResponse.SC_OK);
