8484bde38e51bd2c3971aa2d6f4e22c91f520bb7
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-11-30 16:24:35 +0400

    phy

--- src/com/voc_cards/Voc_cardsServlet.java
+++ src/com/voc_cards/Voc_cardsServlet.java
@@ -16,6 +16,9 @@ public class Voc_cardsServlet extends HttpServlet {
 		ServletContext context = request.getSession().getServletContext();
 		URL resourceUrl = context.getResource("/index.html");
 		InputStream resourceContent = context.getResourceAsStream("/index.html");
+		
+		
+		
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
