da101b0338d091a6bdf228b0e9ca14afab061e8a
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-11-30 18:10:03 +0400

    Change workpath in eclipse.

--- src/idx_coursors/IdxNodeAccessorTest.java
+++ src/idx_coursors/IdxNodeAccessorTest.java
@@ -8,12 +8,8 @@ public class IdxNodeAccessorTest {
   @Test
   public void testNodeRight() throws NodeNoFound, NodeAlreadyExist, NodeIsCorrupted {
     String pathToNode = "./app-folder/bec-node";
-    Optional<ImmutableNodeAccessor> accessor = Optional.absent();
-    try {
-      accessor = Optional.of(IdxNodeAccessor.createImmutableConnection(pathToNode));
-      assertEquals(false, accessor.equals(Optional.absent()));
-    } catch (NodeNoFound e) {
-      assertEquals(accessor, Optional.absent());
-    }
+    Optional<ImmutableNodeAccessor> accessor = 
+    		Optional.of(IdxNodeAccessor.createImmutableConnection(pathToNode));
+	  assertEquals(false, accessor.equals(Optional.absent()));
   }
 }
