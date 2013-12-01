1b5efe196f08fbdc58952cfa767cf241fd4b5b69
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-01 18:08:17 +0400

    save

--- bat.bat
+++ bat.bat
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-git config --global user.email "bb@gmail.com"
-git config --global user.name "bb"
-set HOME=D:/home
->>>>>>> decf462125549a34cd967f8f7f35f6ba72e608d4
+git config --global user.email "igor.a.lugansky@gmail.com"
+git config --global user.name "zaqwes8811"
 cmd &
