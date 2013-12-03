748bcfd21928c9fe308b6b153162becf4703f078
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-03 21:24:01 +0400

    Make accessor for one node.

--- src/read_chain/web_wrapper/ListGetterImpl.java
+++ src/read_chain/web_wrapper/ListGetterImpl.java
@@ -11,57 +11,67 @@ import java.util.*;
 @Deprecated
 public class ListGetterImpl implements ListGetter {
   private final ImmutableList<ImmutableNodeAccessor> NODES;
-  private final ImmutableNodeAccessor ACTIVE_NODE_ACCESSOR;
-  private final GeneratorAnyRandom GENERATOR;
+  private static class OneNodePacker implements ListGetter {
+    private final ImmutableNodeAccessor ACCESSOR_;
+    private final GeneratorAnyRandom GENERATOR_;
+    public OneNodePacker(ImmutableNodeAccessor accessor) {
+      ACCESSOR_ = accessor;
+      List<Integer> distribution = ACCESSOR_.getDistribution();
+      GENERATOR_ = GeneratorAnyRandom.create(distribution);
+    }
 
-  // Index: нужно для маркеровки
-  // Word: само слово
-  // Translates:
-  // Context:
-  @Override
-  public ImmutableList<ImmutableList<ImmutableList<String>>> getPerWordData() {
-	  List<String> rawKeys = new ArrayList<String>();
-	  List<ImmutableList<String>> values = new ArrayList<ImmutableList<String>>();
-	  
-	  // Отправляем только если есть контекст
-	  while (true) {
-	  	boolean finded = false;
-	    Integer currentKey = GENERATOR.getCodeWord();
-	
-	    // Добавляем, только если что-то есть
-	    ImmutableList<String> content = ACTIVE_NODE_ACCESSOR.getContent(currentKey);
-	    if (!content.isEmpty()) {
-	      rawKeys.add("content");
-	      values.add(content);
-	      finded = true;
-	    }
+    // Index: нужно для маркеровки
+    // Word: само слово
+    // Translates:
+    // Context:
+    @Override
+    public ImmutableList<ImmutableList<ImmutableList<String>>> getPerWordData() {
+      List<String> rawKeys = new ArrayList<String>();
+      List<ImmutableList<String>> values = new ArrayList<ImmutableList<String>>();
 
-      if (finded) {
-        ImmutableList<String> translate = ImmutableList.of();
-        if (!translate.isEmpty()) {
-          rawKeys.add("translate");
-          values.add(translate);
+      // Отправляем только если есть контекст
+      while (true) {
+        boolean finded = false;
+        Integer currentKey = GENERATOR_.getCodeWord();
+
+        // Добавляем, только если что-то есть
+        ImmutableList<String> content = ACCESSOR_.getContent(currentKey);
+        if (!content.isEmpty()) {
+          rawKeys.add("content");
+          values.add(content);
+          finded = true;
         }
 
-        // Обязательно!
-        rawKeys.add("word");
-        values.add(ImmutableList.of(ACTIVE_NODE_ACCESSOR.getWord(currentKey)));
-        break;
+        if (finded) {
+          ImmutableList<String> translate = ImmutableList.of();
+          if (!translate.isEmpty()) {
+            rawKeys.add("translate");
+            values.add(translate);
+          }
+
+          // Обязательно!
+          rawKeys.add("word");
+          values.add(ImmutableList.of(ACCESSOR_.getWord(currentKey)));
+          break;
+        }
       }
-  	}
-	  List<ImmutableList<String>> keys = new ArrayList<ImmutableList<String>>();
-    keys.add(ImmutableList.copyOf(rawKeys));
-    return ImmutableList.of(ImmutableList.copyOf(keys), ImmutableList.copyOf(values));
+      List<ImmutableList<String>> keys = new ArrayList<ImmutableList<String>>();
+      keys.add(ImmutableList.copyOf(rawKeys));
+      return ImmutableList.of(ImmutableList.copyOf(keys), ImmutableList.copyOf(values));
+    }
   }
+
+  @Override
+  public ImmutableList<ImmutableList<ImmutableList<String>>> getPerWordData() {
+    ImmutableNodeAccessor accessor = NODES.asList().get(0);
+    ListGetter getter = new OneNodePacker(accessor);
+    return getter.getPerWordData();
+  }
+
 
   public ListGetterImpl(ImmutableList<ImmutableNodeAccessor> nodes) {
     // В try сделать нельзя - компилятор будет ругаться не неинициализованность
     NODES = nodes;  // Должна упасть если при ошибке дошла до сюда
-    ACTIVE_NODE_ACCESSOR = NODES.asList().get(0);
-
-    // Коннектим генератор случайных чисел и акссессор
-    List<Integer> distribution = ACTIVE_NODE_ACCESSOR.getDistribution();
-    GENERATOR = GeneratorAnyRandom.create(distribution);
   }
 }
 
