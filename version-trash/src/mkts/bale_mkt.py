0288d265519f8e39a9e6a9014df25250931603a5
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-04 22:35:07 +0400

    save and push

--- version-trash/src/mkts/bale_mkt.py
+++ version-trash/src/mkts/bale_mkt.py
@@ -65,56 +65,56 @@ class Name  !! no comments
 
   Usige:
     Java:
-    interface IAll extend/*(? это предположение)* / strange_things.IExtractorTextFromResource, strange_things.IExtracitorResourcePlan
-    class MixedClass2 implements strange_things.IExtractorTextFromResource, strange_things.IExtracitorResourcePlan
+    interface IAll extend/*(? это предположение)* / strange_things.ExtractorTextFromResource, strange_things.ExtracitorResourcePlan
+    class MixedClass2 implements strange_things.ExtractorTextFromResource, strange_things.ExtracitorResourcePlan
 """
 
 
-interface strange_things.IExtracitorResourcePlan
+interface strange_things.ExtracitorResourcePlan
     """ Как бы навигационная часть """
     # Спорный вызов. Как он будет использоваться при множественном наследовании?
     # Захватывает точку входа в ресурс (например, файл базы данных)
-    strange_things.IErrorContainer captureRoot(String urlRoot)
+    strange_things.ErrorContainer captureRoot(String urlRoot)
     # Error _freeRoot();  # должен вызывать в деструкторе
 
     # @param url информация о том, как можно добраться до вершины ресурса
     # @return карта, по которой можно передвигаться
-    strange_things.IGraphToText getPlan(String url, strange_things.IErrorContainer err)
+    strange_things.GraphToText getPlan(String url, strange_things.ErrorContainer err)
 
-    strange_things.IGraphToText getPlanFromText(String url, strange_things.IErrorContainer err)
+    strange_things.GraphToText getPlanFromText(String url, strange_things.ErrorContainer err)
 
     # @return просто список "адресов"
     # Обобщить дробилку с рекурсией! Она использовалась для разбора страницы Курсеры
-    List<String> getListAddresses(String url, strange_things.IErrorContainer err)
+    List<String> getListAddresses(String url, strange_things.ErrorContainer err)
 
     # Выделяем адреса без подключения, у нас есть текст, который доставерно
     #   отражает план ресурса
-    List<String> extractListAddressesFromText(String text, strange_things.IErrorContainer err)
+    List<String> extractListAddressesFromText(String text, strange_things.ErrorContainer err)
 
 
-interface strange_things.IExtractorTextFromResource
+interface strange_things.ExtractorTextFromResource
     """ Соединяется с ресурсом и преобразовывает его в текст.
       Thinks:
         Не забывать освободить ресурсы(о реализации), но как быть при множ. наследовании
     """
     # Выделяет весь текст из ресурса по некоторому адресу
     # @param url в переменной все данные, чтобы ресурс мог быть открыт
-    String extract(String url, strange_things.IErrorContainer err)
+    String extract(String url, strange_things.ErrorContainer err)
 
     # Получить из ресурса сразу список единиц контента - предложений, например
-    List<String> contentItemsToList(String url, strange_things.IErrorContainer err)
+    List<String> contentItemsToList(String url, strange_things.ErrorContainer err)
 
-interface strange_things.ITextToText
+interface strange_things.TextToText
     # @param text зашумленнй текст, например html код
     # @return чистый текст
     # ! не возможно разрешить перегрузку
-    String testToText(String text, strange_things.IErrorContainer err)
+    String testToText(String text, strange_things.ErrorContainer err)
 
     # Получить сразу список единиц контента
-    List<String> contentItemsToList(String text, strange_things.IErrorContainer err)
+    List<String> contentItemsToList(String text, strange_things.ErrorContainer err)
 
 
-interface strange_things.IErrorContainer
+interface strange_things.ErrorContainer
     """ 
       Thinks:
         Как я понял в Java нельзя передать примитив по ссылке, исключениями для обработки ошибок
