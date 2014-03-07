80a7479892a072953f79b4f366cce7047b7eb05f
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-07 16:30:29 +0400

    save and push

--- war/controllers.js
+++ war/controllers.js
@@ -1,7 +1,7 @@
 var phonecatApp = angular.module('phonecatApp', []);
  
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
+  $scope.translates = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.'},
     {'name': 'Motorola XOOM™ with Wi-Fi',
@@ -10,7 +10,24 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
      'snippet': 'The Next, Next Generation tablet.'}
   ];
   
-  $scope.go = function() {
-     $scope.phones.push({'name': 'ololo'});
+  $scope.contextOfWord = [
+    {'name': 'Nexus S asfdasdf',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+  
+  $scope.showTranslates = function() {
+    $scope.myValue_tr = false;
+    $scope.myValue_co = true;
   };
+  
+  $scope.showContextItems = function() {
+    $scope.myValue_tr = true;
+    $scope.myValue_co = false;
+  };
+  
+  $scope.myValue_tr = true;
 });
