e8f8dc422855a48c0604c53b85741e5c2a71f44b
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-09 00:20:55 +0400

    save and push

--- src/business/parsers/SubtitlesParserTest.java
+++ src/business/parsers/SubtitlesParserTest.java
@@ -1,59 +1,26 @@
 package business.parsers;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableList;
 import com.google.common.io.Closer;
-import common.Utils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.tika.parser.Parser;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SubtitlesParserTest {
   @Test
   public void testCreate() throws IOException {
-    char SPECIAL_SYMBOL = '’';  // TODO: it's trouble. Константа в коде - юникодная.
+    String filename = "statistic/Frozen.2013.CAMRIP.CHiLLYWiLLY.srt";
 
     // Пока файл строго юникод - UTF-8
     Closer closer = Closer.create();
     try {
-      String filename = "statistic/Frozen.2013.CAMRIP.CHiLLYWiLLY.srt";
       InputStream in = closer.register(new FileInputStream(new File(filename)));
+      Parser parser = new SubtitlesParser();
+      parser.parse(in, null, null, null);
 
-      BufferedReader reader = new BufferedReader(new InputStreamReader(in
-        ,
-        //Charsets.US_ASCII
-        Charsets.UTF_8
-      ));
-
-      String buffer;
-      StringBuilder result = new StringBuilder();
-      while ((buffer = reader.readLine()) != null) {
-        // TODO: not effective
-
-        // Делим пробелами, а по всем пробельным символам
-        // TODO: разделить не только пробелами
-        ImmutableList<String> splitted = ImmutableList.copyOf(
-          Splitter.on(CharMatcher.WHITESPACE)
-            .trimResults()
-            .omitEmptyStrings()
-            .split(buffer));
-
-        // Удаляем просто числа
-        String joinString = Joiner.on("").join(splitted);
-
-        if (!splitted.isEmpty()
-            && !(splitted.size() == 1
-              && StringUtils.isNumeric(joinString)))
-          {
-          buffer = CharMatcher.is(SPECIAL_SYMBOL).replaceFrom(buffer, "'");
-          Utils.print(buffer);
-          result.append(buffer).append('\n');
-        }
-      }
     } catch (Throwable e) { // must catch Throwable
       throw closer.rethrow(e);
     } finally {
