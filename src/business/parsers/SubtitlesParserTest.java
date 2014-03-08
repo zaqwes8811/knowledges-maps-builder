25fb64ccb9bb1c50dad1700dff9cb6cdff4bd5e1
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-08 16:52:11 +0400

    save and push

--- src/business/parsers/SubtitlesParserTest.java
+++ src/business/parsers/SubtitlesParserTest.java
@@ -7,7 +7,32 @@ package business.parsers;
  * Time: 15:29
  * To change this template use File | Settings | File Templates.
  */
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.ParseContext;
 import org.junit.Test;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+class SubtitlesParser implements org.apache.tika.parser.Parser {
+  @Override
+  public Set<MediaType> getSupportedTypes(ParseContext parseContext) {
+    return null;
+  }
+
+  @Override
+  public void parse(
+    InputStream stream, ContentHandler handler,
+    Metadata metadata, ParseContext context)
+    throws IOException, SAXException, TikaException {
+
+  }
+}
 
 public class SubtitlesParserTest {
   @Test
