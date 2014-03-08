81cc49ee29dd64d14d6bdb5013b64c6a8490cacb
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-09 00:20:55 +0400

    save and push

--- src/business/parsers/SubtitlesParser.java
+++ src/business/parsers/SubtitlesParser.java
@@ -1,5 +1,13 @@
 package business.parsers;
 
+import business.nlp.SentencesSplitter;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import common.Utils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
@@ -7,8 +15,12 @@ import org.apache.tika.parser.ParseContext;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class SubtitlesParser implements org.apache.tika.parser.Parser {
@@ -17,11 +29,58 @@ public class SubtitlesParser implements org.apache.tika.parser.Parser {
     return null;
   }
 
+  private final char SPECIAL_SYMBOL = '’';  // TODO: it's trouble. Константа в коде - юникодная.
+  private final char REPLACE_SYMBOL = '\'';
+  // TODO: it's weak. 00:31:19,764 --> 00:31:22,823
+  // TODO: Усилить регулярным выражением
+  private final String TIME_TICKET_SIGN = "-->";
+
   @Override
   public void parse(
-    InputStream stream, ContentHandler handler,
-    Metadata metadata, ParseContext context)
-    throws IOException, SAXException, TikaException {
+      InputStream stream, ContentHandler handler,
+      Metadata metadata, ParseContext context) throws IOException, SAXException, TikaException
+    {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream
+        ,
+        //Charsets.US_ASCII
+        Charsets.UTF_8
+      ));
+
+    String buffer;
+    List<String> result = new ArrayList();
+    while ((buffer = reader.readLine()) != null) {
+      // TODO: not effective
+
+      // Делим пробелами, а по всем пробельным символам
+      // TODO: разделить не только пробелами
+      ImmutableList<String> splitted = ImmutableList.copyOf(
+        Splitter.on(CharMatcher.WHITESPACE)
+          .trimResults()
+          .omitEmptyStrings()
+          .split(buffer));
+
+      // Удаляем просто числа
+      String joinString = Joiner.on("").join(splitted);
+
+      if (!splitted.isEmpty()
+          && !(splitted.size() == 1 && StringUtils.isNumeric(joinString))
+          && !buffer.contains(TIME_TICKET_SIGN))
+        {
+        buffer = CharMatcher.is(SPECIAL_SYMBOL).replaceFrom(buffer, REPLACE_SYMBOL);
+        result.add(buffer);
+      }
+    }
+
+    // Получили список строк.
+    String text = Joiner.on(" ").join(result);
+    ImmutableList<String> sentences = new SentencesSplitter().getSentences(text);
+
+    // Убираем знаки прямой речи
+
+    // Summary
+    for (String sentence : sentences) {
+      Utils.print(sentence);
+    }
 
   }
 }
