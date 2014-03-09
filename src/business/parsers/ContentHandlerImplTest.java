5e3f1675ca5db8675f80e469c733ca3aaadb9cfa
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-09 20:38:50 +0400

    Failed.

--- src/business/parsers/ContentHandlerImplTest.java
+++ src/business/parsers/ContentHandlerImplTest.java
@@ -1,12 +1,24 @@
 package business.parsers;
 
+import com.google.common.base.Charsets;
 import org.junit.Test;
+import org.xml.sax.ContentHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  */
 public class ContentHandlerImplTest {
   @Test
   public void testCharacters() throws Exception {
+    List<String> sink = new ArrayList<String>();
+    ContentHandler handler = new ContentHandlerImpl(sink);
+    String step0 = "hello";
+    char [] bytes = step0.toCharArray();
 
+    handler.characters(bytes, 0, bytes.length);
+
+    assert sink.size() == 1;
   }
 }
