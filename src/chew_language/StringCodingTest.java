79939eec65055b2f99ebbba5e0fc851bb220fea9
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-09 11:05:27 +0400

    save and push

--- src/chew_language/StringCodingTest.java
+++ src/chew_language/StringCodingTest.java
@@ -6,6 +6,8 @@ public class StringCodingTest {
 
   @Test
   public void testCharBuffer() {
-
+    char[] a = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    String b = new String(a);
+    assert b.equals("hello world");
   }
 }
