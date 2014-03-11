f5fb8c0be995162c8541d82bec5f2db4f59e9bca
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-11 19:58:23 +0400

    save and push

--- src/business/mapreduce/CounterMapper.java
+++ src/business/mapreduce/CounterMapper.java
@@ -1,11 +1,20 @@
 package business.mapreduce;
 
 import com.google.common.collect.Multiset;
+import org.checkthread.annotations.NotThreadSafe;
 
 /**
  https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
+
+ Fake MapReduce - try make similar
  */
+@NotThreadSafe
 public class CounterMapper {
+  private final CountReducer reducer_;
+  public CounterMapper(CountReducer reducer) {
+    reducer_ = reducer;
+  }
+
   public void map(String text, Multiset<String> output) {
 
   }
