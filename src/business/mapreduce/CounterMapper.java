bb1040e57dca0589b9e5baf6801f327b08ea7ee0
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-11 19:50:21 +0400

    save and push

--- src/business/mapreduce/CounterMapper.java
+++ src/business/mapreduce/CounterMapper.java
@@ -5,14 +5,8 @@ import com.google.common.collect.Multiset;
 /**
  https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
  */
-public class WordCounter {
-  public void run() {
+public class CounterMapper {
+  public void map(String text, Multiset<String> output) {
 
   }
-  class Map {
-    public void map(String text, Multiset<String> output) {
-
-    }
-  }
-
 }
