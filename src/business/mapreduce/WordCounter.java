688b6a8620222b57075905a0bfefbe0aeb339453
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-11 19:46:28 +0400

    save and push

---
+++ src/business/mapreduce/WordCounter.java
@@ -0,0 +1,18 @@
+package business.mapreduce;
+
+import com.google.common.collect.Multiset;
+
+/**
+ https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
+ */
+public class WordCounter {
+  public void run() {
+
+  }
+  class Map {
+    public void map(String text, Multiset<String> output) {
+
+    }
+  }
+
+}
