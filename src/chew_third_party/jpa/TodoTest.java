0288aeaf2218f9d827fa7541b3f64361c0062a3d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-13 20:39:15 +0400

    Work

---
+++ src/chew_third_party/jpa/TodoTest.java
@@ -0,0 +1,37 @@
+package chew_third_party.jpa;
+
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class TodoTest {
+  private static final String PERSISTENCE_UNIT_NAME = "todos";
+  private static EntityManagerFactory factory;
+
+  @Test
+  public void testInstallation() {
+    factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    EntityManager em = factory.createEntityManager();
+    // read the existing entries and write to console
+    Query q = em.createQuery("select t from Todo t");
+    List<Todo> todoList = q.getResultList();
+    for (Todo todo : todoList) {
+      System.out.println(todo);
+    }
+    System.out.println("Size: " + todoList.size());
+
+    // create new todo
+    em.getTransaction().begin();
+    Todo todo = new Todo();
+    todo.setSummary("This is a test");
+    todo.setDescription("This is a test");
+    em.persist(todo);
+    em.getTransaction().commit();
+
+    em.close();
+  }
+}
