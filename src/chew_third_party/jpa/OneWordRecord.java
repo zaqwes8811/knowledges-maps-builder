d09e88e09a784baa1d03b276d9973e817033c962
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-15 15:24:33 +0400

    Can't connect own class.

--- src/chew_third_party/jpa/OneWordRecord.java
+++
@@ -1,7 +0,0 @@
-package chew_third_party.jpa;
-
-/**
- * Created by кей on 15.03.14.
- */
-public class OneWordRecord {
-}
---
+++ src/chew_third_party/jpa/SentencesTest.java
@@ -0,0 +1,29 @@
+package chew_third_party.jpa;
+
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class SentencesTest {
+  private static final String PERSISTENCE_UNIT_NAME = "sentences";
+  private static EntityManagerFactory factory;
+
+  @Test
+  public void testReadAll() {
+    factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    EntityManager em = factory.createEntityManager();
+
+    Query q = em.createQuery("select t from Sentences t");
+    List<Todo> todoList = q.getResultList();
+    for (Todo todo : todoList) {
+      System.out.println(todo);
+    }
+    System.out.println("Size: " + todoList.size());
+
+    em.close();
+  }
+}
