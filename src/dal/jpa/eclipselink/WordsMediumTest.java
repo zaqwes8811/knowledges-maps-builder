91eb801c74352de60d410278ba9f8da0c143a224
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-28 17:51:19 +0400

    save and push

--- src/dal/jpa/eclipselink/WordsMediumTest.java
+++ src/dal/jpa/eclipselink/WordsMediumTest.java
@@ -36,20 +36,20 @@ public class WordsMediumTest {
     em.getTransaction().begin();
 
     // read the existing entries
-    Query q = em.createQuery("select m from Sentences m");
-    // Persons should be empty
+    Query q = em.createQuery("SELECT m FROM Sentences m");
 
+    // Persons should be empty
     // do we have entries?
     assertTrue(q.getResultList().size() == 0);
     Words word = new Words();
     word.setValue("heart");
-    em.persist(word);
+    em.persist(word);  // TODO: Это обязательно?
 
     Sentences sentence = new Sentences();
     sentence.setSentence("both foul and fair");
     word.getSentences().add(sentence);
     em.persist(sentence);
-    em.persist(word);
+    em.persist(word);  // TODO: еще раз? Да похоже мы к слову что-то добавили
 
 
     // Commit the transaction, which will cause the entity to
@@ -64,6 +64,23 @@ public class WordsMediumTest {
   @After
   public void tearDown() {
     // TODO: Очистить базу данных
+    factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    EntityManager em = factory.createEntityManager();
+
+    // TODO: Порядок важен. Но как быть при двунаправленной связи.
+    em.getTransaction().begin();
+    Query q = em.createQuery("DELETE FROM Words");
+    int deleted = q.executeUpdate();
+    assert deleted == 1;
+    em.getTransaction().commit();
+
+    em.getTransaction().begin();
+    q = em.createQuery("DELETE FROM Sentences");
+    deleted = q.executeUpdate();
+    assert deleted == 1;
+    em.getTransaction().commit();
+
+    em.close();
   }
 
   @Test
