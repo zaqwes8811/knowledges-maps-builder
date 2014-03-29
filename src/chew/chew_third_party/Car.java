eccbfc69c36920abfade557f374aeb6662522f7d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-29 12:11:58 +0400

    Different not compatible versions.

--- src/chew/chew_third_party/Car.java
+++ src/chew/chew_third_party/Car.java
@@ -2,20 +2,11 @@ package chew.chew_third_party;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
-import com.googlecode.objectify.annotation.Ignore;
+import com.googlecode.objectify.annotation.Index;
 
 @Entity
 public class Car {
     @Id Long id;
-    String vin;
+    @Index String license;
     int color;
-    byte[] rawData;
-    @Ignore String irrelevant;
-
-    private Car() {}
-    
-    public Car(String vin, int color) {
-        this.vin = vin;
-        this.color = color; 
-    }
 }
