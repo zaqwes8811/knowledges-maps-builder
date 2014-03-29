270a43b627f9d03918cbb6bc2d696d7ec24120af
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-29 14:38:27 +0400

    save and push

--- src/chew/chew_third_party/gae/LocalDatastoreTest.java
+++ src/chew/chew_third_party/gae/LocalDatastoreTest.java
@@ -1,4 +1,6 @@
 // https://developers.google.com/appengine/docs/java/tools/localunittesting?csw=1#Introducing_the_Java_Testing_Utilities
+//
+// TODO: in-depth - https://developers.google.com/appengine/articles/datastore/overview !!!
 package chew.chew_third_party.gae;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
