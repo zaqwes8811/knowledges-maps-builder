485ea0284a97babd2d54d5dfacfeec1b784f3aab
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-29 13:29:27 +0400

    save

---
+++ src/chew/chew_third_party/gae/OfyService.java
@@ -0,0 +1,21 @@
+package chew.chew_third_party.gae;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+public class OfyService {
+  static {
+    factory().register(Car.class);
+    //factory().register(OtherThing.class);
+    //...etc
+  }
+
+  public static Objectify ofy() {
+    return ObjectifyService.begin();
+  }
+
+  public static ObjectifyFactory factory() {
+    return ObjectifyService.factory();
+  }
+}
