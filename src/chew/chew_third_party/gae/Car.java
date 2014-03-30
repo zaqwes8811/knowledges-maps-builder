694d2016945c1480c07e3c2600feb8b962f5149d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-03-30 14:19:32 +0400

    On add test case all broucked

--- src/chew/chew_third_party/gae/Car.java
+++ src/chew/chew_third_party/gae/Car.java
@@ -1,12 +1,18 @@
 package chew.chew_third_party.gae;
 
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Load;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Car {
     @Id Long id;
     @Index String license;
     int color;
+    //@Load List<Ref<Engine>> owners = new ArrayList<Ref<Engine>>();
 }
