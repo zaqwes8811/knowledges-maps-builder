c9e72d1cd1f007e4ce095c14fcfe908853aa996d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-01 22:11:22 +0300

    phy

---
+++ sandbox/src/third_party_tests/gae/Car.java
@@ -0,0 +1,18 @@
+package third_party_tests.gae;
+
+import com.googlecode.objectify.Ref;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Load;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Car {
+    @Id Long id;
+    @Index String license;
+    int color;
+    @Load List<Ref<Engine>> owners = new ArrayList<Ref<Engine>>();
+}
