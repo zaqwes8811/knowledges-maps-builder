cb0b85c269810088f30ad8eb4775c213d3471d3a
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-07 23:48:17 +0300

    save

--- sandbox/src/ads/Notes.java
+++ sandbox/src/ads/Notes.java
@@ -4,6 +4,8 @@ package ads;
 // http://www.vogella.com/tutorials/JavaDatastructures/article.html
 //
 // http://docs.oracle.com/javase/tutorial/collections/implementations/index.html
+//
+// http://java.dzone.com/articles/hashmap-internal
 
 /**
  * Created by zaqwes on 5/7/14.
