0692cc5a6a2d3574c7e2163e5c1e1cf19271a2e4
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-08 23:44:02 +0300

    phy

--- src/business/mapreduce/old/NewMapper.java
+++ src/business/mapreduce/old/NewMapper.java
@@ -1,4 +1,4 @@
-package business.mapreduce;
+package business.mapreduce.old;
 
 
 import common.InnerReuse;
--- src/business/mapreduce/old/WordsProcessorTest.java
+++ src/business/mapreduce/old/WordsProcessorTest.java
@@ -1,4 +1,4 @@
-package business.mapreduce;
+package business.mapreduce.old;
 
 import com.google.common.collect.Multimap;
 import hided.jobs_processors.JobsFormer;
