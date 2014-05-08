76fd6a0aa353b0093901ae938784de25320d75c4
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-09 00:14:32 +0300

    save

---
+++ src/dal/gae_kinds/ContentItem.java
@@ -0,0 +1,13 @@
+package dal.gae_kinds;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+/**
+ * Created by zaqwes on 5/9/14.
+ */
+@Entity
+public class ContentItem {
+  @Id
+  Long id;
+}
