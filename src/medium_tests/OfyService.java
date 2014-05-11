24c4d9feb43f3dbb2e9f80f4262f9d2a36411c62
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-12 01:20:02 +0300

    Persist page.

--- src/medium_tests/OfyService.java
+++ src/medium_tests/OfyService.java
@@ -4,10 +4,12 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
 import dal.gae_kinds.ContentItem;
+import dal.gae_kinds.ContentPage;
 
 public class OfyService {
   static {
     factory().register(ContentItem.class);
+    factory().register(ContentPage.class);
     //...etc
   }
 
