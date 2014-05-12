c879a70cec2ecc48585bd225cdbd5f64c328beda
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-13 01:30:10 +0300

    Prepare to extract class of WorkPage.

--- src/dal/gae_kinds/OfyService.java
+++ src/dal/gae_kinds/OfyService.java
@@ -7,7 +7,7 @@ import com.googlecode.objectify.ObjectifyService;
 public class OfyService {
   static {
     factory().register(ContentItem.class);
-    factory().register(ContentPage.class);
+    factory().register(ContentPageKind.class);
     factory().register(Word.class);
     //...etc
   }
