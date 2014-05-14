9048fe5b7478720f172f00538eba6441e64f819d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-15 00:29:02 +0300

    save

--- src/business/math/GeneratorDistributionsImplTest.java
+++ src/business/math/GeneratorDistributionsImplTest.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
+
 /**
  * Created by zaqwes on 5/12/14.
  */
@@ -14,12 +16,11 @@ public class GeneratorDistributionsImplTest {
   public void testCreate() throws GeneratorDistributionExc {
     ArrayList<Integer> distribution = new ArrayList<Integer>(Arrays.asList(1, 6, 0, 14, 5, 7));
 
-    GeneratorDistributionsImpl generator = GeneratorDistributionsImpl.create(distribution);
+    GeneratorDistributions generator = GeneratorDistributionsImpl.create(distribution);
     List<Integer> experiment = new ArrayList<Integer>();
     for (int i = 0; i < 10000; ++i)
-      //experiment.add(distribution.get(generator.getPosition()));
-      experiment.add((generator.getPosition()));
+      experiment.add(generator.getPosition());
 
-    System.out.println(experiment);
+    assertFalse(experiment.contains(new Integer(distribution.indexOf(0))));
   }
 }
