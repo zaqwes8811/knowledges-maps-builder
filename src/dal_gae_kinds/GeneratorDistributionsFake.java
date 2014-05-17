3bad6f682c4435bdc36aca447839b4b03a6134c2
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-17 22:54:30 +0300

    save

--- src/dal_gae_kinds/GeneratorDistributionsFake.java
+++ src/dal_gae_kinds/GeneratorDistributionsFake.java
@@ -1,7 +1,6 @@
 package dal_gae_kinds;
 
 import business.math.GeneratorAnyDistribution;
-import business.math.GeneratorDistributions;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
@@ -11,7 +10,7 @@ import java.util.ArrayList;
  * Created by zaqwes on 5/15/14.
  */
 @Entity
-public class GeneratorDistributionsFake implements GeneratorDistributions {
+public class GeneratorDistributionsFake implements ContentPage.GeneratorDistributions {
   /// Persist
   @Id
   Long id;
@@ -24,4 +23,16 @@ public class GeneratorDistributionsFake implements GeneratorDistributions {
 
   @Override
   public void reloadGenerator(ArrayList<GeneratorAnyDistribution.DistributionElement> distribution) { }
+
+  @Override
+  public void disablePoint(Integer idx) {
+    // TODO: Проверка границ - это явно ошибка
+
+    // TODO: Похоже нужна non-XG - транзакция. Кажется может возникнуть исключение.
+  }
+
+  @Override
+  public void enablePoint(Integer idx) {
+
+  }
 }
