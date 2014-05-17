4bc124d67f4e8ad0b8c2aa62bf72981b0dda2126
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-18 02:28:07 +0300

    Make in data size smaller for test.

---
+++ src/common/Printer.java
@@ -0,0 +1,10 @@
+package common;
+
+/**
+ * Created by zaqwes on 5/18/14.
+ */
+public class Printer {
+  public Printer(Object obj) {
+    System.out.println(obj);
+  }
+}
--- src/dal_gae_kinds/ContentPage.java
+++ src/dal_gae_kinds/ContentPage.java
@@ -58,12 +58,17 @@ public class ContentPage {
     for (final ContentItem item: items) this.items.add(Key.create(item));
   }
 
+  // TODO: Кеш для контекста. Можно по ключам, но подгрузка поштучная.
+  // TODO: Холодный старт может быть медленным. Хорошо бы сделат общий кеш, но как?
+  //@Unindex final LoadingCache<Key<ContentItem>, ContentItem> contentCache = CacheBuilder.newBuilder()
+  //  .build();
+
   @Unindex
   private final LoadingCache<Integer, Optional<Word>> wordsCache = CacheBuilder.newBuilder()
     .expireAfterAccess(10, TimeUnit.MINUTES)  // TODO: Make by size.
     .build(
       new CacheLoader<Integer, Optional<Word>>() {
-        public Optional<Word> load(Integer key) { // no checked exception
+        public Optional<Word> load(Integer key) { // TODO: no checked exception
           //
           return Optional.absent();//createExpensiveGraph(key);
         }
@@ -72,7 +77,9 @@ public class ContentPage {
   public Pair<Optional<Word>, Optional<ArrayList<ContentItem>>> get(Integer position) {
     try {
       // TODO: Получение элементов контекста
-      return Pair.with(wordsCache.get(position), Optional.<ArrayList<ContentItem>>absent());
+      Optional<Word> word = wordsCache.get(position);
+
+      return Pair.with(word, Optional.<ArrayList<ContentItem>>absent());
     } catch (ExecutionException e) {
       throw new RuntimeException(e.getCause());
     }
@@ -118,11 +125,4 @@ public class ContentPage {
 
     // TODO: Получать распределение, иначе как узнаем как разрешить точку обратно.
   }
-
-  //@NotImmutable
-  //public final class Sample {
-  //  public final Optional<Word> word;
-  //  public final Optional<ArrayList<ContentItem>> items;
-  //  public Sample(Optional<Word> word, )
-  //}
 }
