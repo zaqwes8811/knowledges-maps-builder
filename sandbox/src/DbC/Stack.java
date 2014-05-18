539f12c1dbfe2349b6cf81b573072f3f0a553ace
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-18 20:59:02 +0400

    save

--- sandbox/src/DbC/Stack.java
+++ sandbox/src/DbC/Stack.java
@@ -3,10 +3,13 @@ package DbC;
 // https://code.google.com/p/cofoja/wiki/AddContracts
 
 import com.google.java.contract.Invariant;
+import com.google.java.contract.Requires;
 
-@Invariant("size() >= 0")
+@Invariant("size() >= 0")  // step 2
 interface Stack<T> {
-  public int size();
+  public int size();  // step 1
+
+  @Requires("size() >= 1")  // step 3
   public T pop();
   public void push(T obj);
 }
