510ed9f5eeb2020f6f0faa607b1aae6a8f504179
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-18 21:06:36 +0400

    Befor test Stack.

--- sandbox/src/DbC/Stack.java
+++ sandbox/src/DbC/Stack.java
@@ -2,6 +2,7 @@ package DbC;
 
 // https://code.google.com/p/cofoja/wiki/AddContracts
 
+import com.google.java.contract.Ensures;
 import com.google.java.contract.Invariant;
 import com.google.java.contract.Requires;
 
@@ -9,7 +10,22 @@ import com.google.java.contract.Requires;
 interface Stack<T> {
   public int size();  // step 1
 
+  @Requires("size() >= 1")  // step 5
+  public T peek();  // step 5
+
+
   @Requires("size() >= 1")  // step 3
+  //@Ensures("size() == old(size()) - 1")  // step 4
+  @Ensures({  // step 6
+    "size() == old(size()) - 1",
+    "result == old(peek())"
+  })
   public T pop();
+
+  //@Ensures("size() == old(size()) + 1")  // step 4
+  @Ensures({  // step 6
+    "size() == old(size()) + 1",
+    "peek() == old(obj)"
+  })
   public void push(T obj);
 }
