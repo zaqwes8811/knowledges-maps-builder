909a41b05f72a225ad86de79c052422fdde26f5a
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-18 21:06:36 +0400

    Befor test Stack.

---
+++ sandbox/src/DbC/ArrayListStackTest.java
@@ -0,0 +1,13 @@
+package DbC;
+
+import org.junit.Test;
+
+/**
+ * Created by zaqwes on 5/18/14.
+ */
+public class ArrayListStackTest {
+  @Test
+  public void testPop() throws Exception {
+
+  }
+}
