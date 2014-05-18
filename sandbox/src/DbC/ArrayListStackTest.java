85fc2d31af2dc0c3347cd5067176caf951a50039
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-05-18 21:28:08 +0400

    save

--- sandbox/src/DbC/ArrayListStackTest.java
+++ sandbox/src/DbC/ArrayListStackTest.java
@@ -3,6 +3,12 @@ package DbC;
 import org.junit.Test;
 
 // On cofoja in IDEA http://java.dzone.com/articles/using-google-contracts-java
+// http://habrahabr.ru/post/139206/ - нужно почитать, не запустил сходу
+//
+// TODO: узнать про annotation processing!
+//
+// еще реализация DbC
+// http://docs.oracle.com/javase/6/docs/technotes/guides/language/assert.html#usage-conditions
 
 public class ArrayListStackTest {
   @Test
