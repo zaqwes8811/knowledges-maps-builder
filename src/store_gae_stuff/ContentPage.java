09c9502762f8d5a6af9e5d628b1fd825b12ba7c1
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-09 18:15:03 +0400

    ref

--- src/store_gae_stuff/ContentPage.java
+++ src/store_gae_stuff/ContentPage.java
@@ -1,41 +1,38 @@
-package store_gae_stuff;
-
-import com.google.appengine.repackaged.org.apache.http.annotation.NotThreadSafe;
-import com.google.common.base.Optional;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableList;
-import com.googlecode.objectify.Key;
-import com.googlecode.objectify.annotation.*;
-import org.javatuples.Pair;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-
-import static store_gae_stuff.OfyService.ofy;
-
 // Tasks:
 //   Сперва подключить кеш,
 //   Затем думать о распределении
 //   Затем думать об удалении.
 
 // http://www.oracle.com/technetwork/java/javase/documentation/index-137868.html#styleguide
-
+// TODO: http://www.oracle.com/technetwork/articles/marx-jpa-087268.html
+// TODO: скрыть персистентность в этом классе, пусть сам себя сохраняет и удаляет.
+// TODO: Функция очистки данных связанных со страницей, себя не удаляет.
+// не хочется выносить ofy()... выше. Но может быть, если использовать класс пользователя, то он может.
 /**
  * About:
  *   Отражает один элемент данный пользователя, например, один файл субтитров.
  */
+
+package store_gae_stuff;
+
+import com.google.appengine.repackaged.org.apache.http.annotation.NotThreadSafe;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Load;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static store_gae_stuff.OfyService.ofy;
+
 @NotThreadSafe
 @Entity
 public class ContentPage {
-  /// Persist
-  // TODO: http://www.oracle.com/technetwork/articles/marx-jpa-087268.html
-  // TODO: скрыть персистентность в этом классе, пусть сам себя сохраняет и удаляет.
-  // не хочется выносить ofy()... выше. Но может быть, если использовать класс пользователя, то он может.
   @Id
   Long id;
 
@@ -48,7 +45,6 @@ public class ContentPage {
   @Load
   List<Key<ContentItem>> items = new ArrayList<Key<ContentItem>>();
 
-  // MUST BE!
   private ContentPage() { }
 
   // TODO: Добавить оценки текста
@@ -57,21 +53,45 @@ public class ContentPage {
   @Load
   List<Key<GeneratorDistributions>> distributions = new ArrayList<Key<GeneratorDistributions>>();
 
-  /// Own
-  // Странице никчему знать про детали интерфейса генераторов
+  // генератор должен быть уже сохранен
+  public void addGenerator() {
+    //assert
+  }
 
+  // Own
+  // Странице никчему знать про детали интерфейса генераторов
   // TODO: как быть с аргументами?
   // https://code.google.com/p/cofoja/wiki/HowtoWriteGoodContracts
-  //@Requires("name != null")  // пока без проверки, но возможно удобно - визуально видно
   public ContentPage(String name, List<ContentItem> items, List<WordItem> words) {
-    if (name == null)
-      throw new IllegalArgumentException("Broken precondition on building page.");
-
-    this.name = name;
+    this.name = Optional.of(name).get();
     for (final WordItem word: words) this.words.add(Key.create(word));
     for (final ContentItem item: items) this.items.add(Key.create(item));
   }
 
+  // About: Возвращать частоты, сортированные по убыванию.
+  public ImmutableList<Integer> getSortedFrequencies() {
+    // TODO: Отосортировать при выборке если можно
+    List<WordItem> wordItems = ofy().load().type(WordItem.class).filterKey("in", this.words).list();
+
+    // Сортируем - элементы могут прийти в случайном порядке
+    // TODO: может при запросе можно отсортировать?
+    Collections.sort(wordItems, WordItem.createFrequencyComparator());
+    Collections.reverse(wordItems);
+
+    // Формируем результат
+    ArrayList<Integer> distribution = new ArrayList<Integer>();
+    for (final WordItem word : wordItems)
+      distribution.add(word.getRawFrequency());
+
+    return ImmutableList.copyOf(distribution);
+  }
+
+  public List<Key<ContentItem>> getItems() {
+    return items;
+  }
+}
+
+/*
   @Unindex
   private final LoadingCache<Integer, Optional<WordItem>> wordsCache = CacheBuilder.newBuilder()
     .expireAfterAccess(10, TimeUnit.MINUTES)  // TODO: Make by size.
@@ -92,26 +112,4 @@ public class ContentPage {
       throw new RuntimeException(e.getCause());
     }
   }
-
-  // About: Возвращать частоты, сортированные по убыванию.
-  public ImmutableList<Integer> getSortedFrequencies() {
-    // TODO: Отосортировать при выборке если можно
-    List<WordItem> words = ofy().load().type(WordItem.class).filterKey("in", this.words).list();
-
-    // Сортируем
-    Collections.sort(words, WordItem.createFrequencyComparator());
-    Collections.reverse(words);
-
-    // Формируем результат
-    ArrayList<Integer> distribution = new ArrayList<Integer>();
-    for (final WordItem word : words) distribution.add(word.getRawFrequency());
-
-    return ImmutableList.copyOf(distribution);
-  }
-
-  // TODO: Функция очистки данных связанных со страницей, себя не удаляет.
-
-  public List<Key<ContentItem>> getItems() {
-    return items;
-  }
-}
+*/
