397de696f6b3b371fd4dfb2d6f54b2068324585e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-11 21:57:35 +0400

    looks better. change shema.

--- src/servlets/FakeAppWrapper.java
+++ src/servlets/FakeAppWrapper.java
@@ -10,42 +10,14 @@ import store_gae_stuff.fakes.BuilderOneFakePage;
 
 public class FakeAppWrapper {
 	public FakeAppWrapper() {
-	// store page
-    String activePageName = "Korra";
-    
-    // store
-    ContentPageKind page = new BuilderOneFakePage().buildContentPage("Korra");
-    ActiveDistributionGenKind gen = ActiveDistributionGenKind.create(page.getRawDistribution());
-    ofy().save().entity(gen).now();
-    page.setGenerator(gen);
-    ofy().save().entity(page).now();
-    
+		{
+			
+		}
+ 
     // read    
     {
-	    // FIXME: генератор не загружен из хранилища!
-	    List<ContentPageKind> loadedPage = 
-	    		ofy().load().type(ContentPageKind.class)activePageName.list();
-	    
-	    if (loadedPage.size() > 1 || loadedPage.size() == 0) {
-	 	    throw new RuntimeException();
-	 	  }
-	    
-	    loadedPage.get(0);
-	    
-	    //if (loadedPage.size() > 1 || loadedPage.size() == 0) {
-	   // 	throw new RuntimeException();
-	    //}
-	    
-	    /*
-	    if (!(loadedPage.get(0).g.getId() == page.g.getId()))
-	    	throw new RuntimeException();
-	    */
-	    
-	    //ActiveDistributionGenKind gens = 
-	    //		ofy().load().type(ActiveDistributionGenKind.class).id(page.g.getId()).now();
-	    
-	   // if (!gens.id.equals(gen.id))
-	    	//throw new RuntimeException();
+  
+
     }
 	}
 }
--- src/servlets/research/GetDistribution.java
+++ src/servlets/research/GetDistribution.java
@@ -1,20 +1,20 @@
 package servlets.research;
 
-import com.google.appengine.labs.repackaged.com.google.common.base.Optional;
-import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.List;
 
-import servlets.FakeAppWrapper;
-import store_gae_stuff.ActiveDistributionGenKind;
-import store_gae_stuff.ContentPageKind;
-
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.util.List;
+import com.googlecode.objectify.Key;
 
+import servlets.FakeAppWrapper;
+import store_gae_stuff.ContentPageKind;
+import store_gae_stuff.EasyKind;
+import store_gae_stuff.fakes.BuilderOneFakePage;
 import static store_gae_stuff.OfyService.ofy;
 
 public class GetDistribution extends HttpServlet {
@@ -25,6 +25,37 @@ public class GetDistribution extends HttpServlet {
   static private FakeAppWrapper w; 
   static {
   	w = new FakeAppWrapper();
+  	
+  	
+  }
+  
+  @Override
+  public void init(ServletConfig config) {
+  	/*EasyKind k0 = new EasyKind();
+  	EasyKind k1 = new EasyKind();
+  	
+  	// они не удаляются
+  	ofy().save().entity(k1).now();
+  	k0.k = Key.create(k1);
+  	ofy().save().entity(k0).now();
+  	
+  	
+  	List<EasyKind> l = ofy().load().type(EasyKind.class).list();
+  	l.size();
+  	
+  	System.out.println(l);
+  	*/
+  	
+  	// Own tables
+  	
+  	String activePageName = "Korra";
+  	ContentPageKind p0 = new BuilderOneFakePage().buildContentPage(activePageName);
+  	ofy().save().entity(p0).now();
+  	
+  	List<ContentPageKind> pages = 
+  			ofy().load().type(ContentPageKind.class).list();
+  	
+  	System.out.println(pages);
   }
 
 	@Override
@@ -32,7 +63,7 @@ public class GetDistribution extends HttpServlet {
     HttpServletRequest request,
     HttpServletResponse response) throws ServletException, IOException
   {
-    String activePageName = "Korra";
+    /*String activePageName = "Korra";
     ContentPageKind loadedPage =
       ofy().load().type(ContentPageKind.class).filter("name = ", activePageName).first().now();
     
@@ -40,7 +71,7 @@ public class GetDistribution extends HttpServlet {
     
     if (gen == null) {
     	throw new IllegalStateException();
-    }
+    }*/
 
     String jsonResponse = "";//new Gson().toJson(gen.getDistribution());
     
@@ -49,5 +80,8 @@ public class GetDistribution extends HttpServlet {
     
     response.setCharacterEncoding("UTF-8");
     response.getWriter().println(jsonResponse);
+    
+    List<Key<ContentPageKind>> keys = ofy().load().type(ContentPageKind.class).keys().list();
+    ofy().delete().keys(keys);
   }
 }
