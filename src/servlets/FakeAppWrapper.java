268f505060d98363e96e1a5537e250a3a4d7b143
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-11 19:40:51 +0400

    Store troubles.

--- src/servlets/FakeAppWrapper.java
+++ src/servlets/FakeAppWrapper.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import static store_gae_stuff.OfyService.ofy;
+
+import java.util.List;
+
 import store_gae_stuff.ActiveDistributionGenKind;
 import store_gae_stuff.ContentPageKind;
 import store_gae_stuff.fakes.BuilderOneFakePage;
@@ -13,5 +16,24 @@ public class FakeAppWrapper {
     ofy().save().entity(gen).now();
     page.setGenerator(gen);
     ofy().save().entity(page).now();
+    
+    // FIXME: генератор не загружен из хранилища!
+    String activePageName = "Korra";
+    ContentPageKind loadedPage =
+      ofy().load().type(ContentPageKind.class).filter("name = ", activePageName).first().now();
+    
+    assert loadedPage != null;
+    
+    ActiveDistributionGenKind gen_ = loadedPage.getGenerators();
+    
+    if (gen_ == null)
+    	throw new RuntimeException();
+    
+    //if (gen_.size() != 1)
+    //	throw new RuntimeException();
+    
+    ActiveDistributionGenKind f = gen_;
+    int i = 0;
+    f.getDistribution();
 	}
 }
