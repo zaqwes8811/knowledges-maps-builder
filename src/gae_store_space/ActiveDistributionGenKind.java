98e9e6248d053ef9c4312677ead098982595256e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-14 15:39:52 +0400

    save

--- src/gae_store_space/ActiveDistributionGenKind.java
+++ src/gae_store_space/ActiveDistributionGenKind.java
@@ -7,6 +7,7 @@ import net.jcip.annotations.NotThreadSafe;
 
 
 
+
 //import com.google.appengine.repackaged.org.apache.http.annotation.NotThreadSafe;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
@@ -20,6 +21,7 @@ import com.googlecode.objectify.annotation.Unindex;
 import core.math.DistributionElement;
 import core.math.GeneratorAnyDistribution;
 import frozen.dal.accessors_text_file_storage.OutOfRangeOnAccess;
+import gae_store_space.fakes.BuilderOneFakePage;
 
 // About:
 //   Класс способен генерировать последовательности любого дискретного распределения
@@ -107,7 +109,9 @@ public class ActiveDistributionGenKind //implements DistributionGen  // no way
   }
 
   
-  private ActiveDistributionGenKind() { }
+  private ActiveDistributionGenKind() { 
+  	name = BuilderOneFakePage.defaultGenName;
+  }
   
   // похоже при восстановлении вызывается он
   // TODO: момент похоже скользкий - а будет ли распределение инициализировано?
