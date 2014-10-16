d37142baa15bbb069a4abc081bd98483343f31d2
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-16 17:06:15 +0400

    save

--- src/servlets/FileAccepter.java
+++ src/servlets/FileAccepter.java
@@ -1,18 +1,19 @@
 package servlets;
 
-import gae_store_space.ContentPageKind;
-import gae_store_space.fakes.BuilderOneFakePage;
 import gae_store_space.fakes.FakeAppWrapper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import servlets.protocols.PathValue;
 import net.jcip.annotations.NotThreadSafe;
-import servlets.protocols.WordDataValue;
 
 import com.google.gson.Gson;
 
@@ -22,18 +23,35 @@ public class FileAccepter extends HttpServlet {
   
 	private FakeAppWrapper app = FakeAppWrapper.getInstance(); 
 
+	// http://commons.apache.org/proper/commons-fileupload/using.html
 	@Override
-  public void doGet(
+  public void doPost(
     HttpServletRequest request,
     HttpServletResponse response) throws ServletException, IOException
   {
 
-    response.setContentType("text/html");
-    response.setStatus(HttpServletResponse.SC_OK);
-    
-    String jsonResponse = new Gson().toJson(9);
+		try {
+	  	BufferedReader reader = 
+	  			new BufferedReader(new InputStreamReader(request.getInputStream()));
+	
+	  	ArrayList<String> lines = new ArrayList<String>();
+	  	String line;
+	    while ((line = reader.readLine()) != null) {
+	    	lines.add(line);
+	    }
+	    
+	    // purge from headers and bottoms
+	  	
+	  	// response
+	  	response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_OK); 
+	    
+	    String jsonResponse = new Gson().toJson(9);
 
-    response.setCharacterEncoding("UTF-8");
-    response.getWriter().println(jsonResponse);
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().println(jsonResponse);
+  	} catch (IOException e) {
+  		throw new IllegalStateException(e);
+  	}
   }
 }
