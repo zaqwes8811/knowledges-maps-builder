1a213de819a7270e09a3c6d5326f0c0970217f3d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-10-18 15:43:16 +0400

    save

--- src/ManageEmployee.java
+++ src/ManageEmployee.java
@@ -1,12 +1,14 @@
 import java.util.List; 
 import java.util.Date;
 import java.util.Iterator; 
- 
+
 import org.hibernate.HibernateException; 
 import org.hibernate.Session; 
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import sand.hibernate.Employee;
 
 public class ManageEmployee {
    private static SessionFactory factory; 
