78ecdbf66b747b0222747ef939551207db49c85f
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-03 13:54:31 +0400

    save

---
+++ impls/vcards-gae/src/pipeline/statistics_collectors/StatisticCollector.java
@@ -0,0 +1,27 @@
+package pipeline.statistics_collectors;
+
+import gae_store_space.SentenceKind;
+
+import java.util.ArrayList;
+
+import pipeline.mapreduce.CountReducer;
+import pipeline.mapreduce.CountReducerImpl;
+import pipeline.mapreduce.CounterMapper;
+import pipeline.mapreduce.CounterMapperImpl;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+// не очень хорошо - трудно будет параметризовать
+public class StatisticCollector {
+	public Multimap<String, SentenceKind> buildHisto(ArrayList<SentenceKind> items) {
+  	Multimap<String, SentenceKind> histo = HashMultimap.create();
+    
+    CountReducer reducer = new CountReducerImpl(histo);
+    CounterMapper mapper = new CounterMapperImpl(reducer);
+
+    mapper.map(items);
+    
+    return histo;
+  }
+}
