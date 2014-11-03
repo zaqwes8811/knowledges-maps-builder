ff5c01433196d2ab879674e2e0ac830e75695303
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-03 17:35:51 +0400

    extract estimators

---
+++ impls/vcards-gae/src/pipeline/estimators/ImportanceProcessor.java
@@ -0,0 +1,11 @@
+package pipeline.estimators;
+
+import gae_store_space.SentenceKind;
+
+import java.util.Set;
+
+public class ImportanceProcessor {
+	public Integer process(Integer freq, Set<SentenceKind> s) {
+		return freq;
+	}
+};
