9ffd414df1a9dff142fdead10a11b40f635273cb
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-04 16:27:58 +0400

    save

--- impls/vcards-gae/src/gae_store_space/AppInstance.java
+++ impls/vcards-gae/src/gae_store_space/AppInstance.java
@@ -82,7 +82,7 @@ public class AppInstance {
 			TextPipeline processor = new TextPipeline();
 	  	PageKind page = processor.pass(name, text);  
 	  	
-	  	GeneratorKind g = GeneratorKind.create(page.getRawDistribution(), TextPipeline.defaultGenName);
+	  	GeneratorKind g = GeneratorKind.create(page.getImportanceDistribution(), TextPipeline.defaultGenName);
 	  	g.syncCreateInStore(); 
 	  	
 	  	page.addGenerator(g);
