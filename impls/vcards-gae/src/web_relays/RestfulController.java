c011b6eb05fe6d9be40f7cb1139ead45e7dc929e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-08 14:53:17 +0400

    point

--- impls/vcards-gae/src/web_relays/RestfulController.java
+++ impls/vcards-gae/src/web_relays/RestfulController.java
@@ -4,29 +4,21 @@ import gae_store_space.AppInstance;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//res.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
 @Controller
 @RequestMapping("/cont")
-public class RestController {	
-	//private AppInstance app = AppInstance.getInstance();
-	
-	public RestController(){
-	   System.out.println("init RestController");
-	}
-	
-	//this method responses to GET request http://localhost/spring-mvc-json/rest/cont
-	// return Person object as json
+public class RestfulController {	
+	private AppInstance app = AppInstance.getInstance();
 	
 	@RequestMapping(value="plot", method = RequestMethod.GET)
 	public @ResponseBody List<Integer> get(HttpServletRequest request, HttpServletResponse res) {
@@ -44,19 +36,8 @@ public class RestController {
 		 person.setName("hmk");
 		 */
 		 
-		 //res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-		ArrayList<Integer> r = new ArrayList<Integer>();
+		 ArrayList<Integer> r = new ArrayList<Integer>();
 		
 		 return r;
-	}
-	
-	//this method response to POST request http://localhost/spring-mvc-json/rest/cont/person
-	// receives json data sent by client --> map it to Person object
-	// return Person object as json
-	@RequestMapping(value="person", method = RequestMethod.POST)
-	public @ResponseBody Person post( @RequestBody final  Person person) {    
-	
-	   System.out.println(person.getId() + " " + person.getName());
-	   return person;
 	}
 }
