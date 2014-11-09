d1bc49c8836c80ed5fb02a72829f193d261327fa
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-09 15:40:15 +0400

    save

--- impls/vcards-gae/src/pipeline/mapreduce/CounterMapperImpl.java
+++ impls/vcards-gae/src/pipeline/mapreduce/CounterMapperImpl.java
@@ -1,14 +1,13 @@
 package pipeline.mapreduce;
 
 
-import org.checkthread.annotations.NotThreadSafe;
-import org.tartarus.snowball.ext.englishStemmer;
-
-import pipeline.nlp.SentenceTokenizer;
-
 import gae_store_space.SentenceKind;
 
 import java.util.List;
+
+import org.checkthread.annotations.NotThreadSafe;
+
+import pipeline.nlp.SentenceTokenizer;
 
 /**
  https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
@@ -26,26 +25,14 @@ public class CounterMapperImpl implements CounterMapper {
   private void emit(String key, SentenceKind value) {
     reducer_.reduce(key, value);
   }
-  
-  englishStemmer stemmer = new englishStemmer();
-  
-  private Word getWord(String value) {
-  	String lowWord = value.toLowerCase();
-  	stemmer.setCurrent(lowWord);
-    if (stemmer.stem()) {
-      return new Word(stemmer.getCurrent(), value);  
-    } else {
-      return new Word(value, value); 
-    }
-  }
-  
+   
   @Override
   public void map(List<SentenceKind> contentItemKinds) {
     SentenceTokenizer tokenizer = new SentenceTokenizer();
     for (SentenceKind item : contentItemKinds) {
       List<String> tokens = tokenizer.getWords(item.getSentence());
       for (String token: tokens) {
-        Word w = getWord(token);
+        Word w = Word.build(token);
         emit(w.stem, item);
       }
       
