8feb3e6dabf85621984b17b3206eca6e8412ab47
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-15 19:35:31 +0300

    save

--- impls/vcard-gae-maven/src/main/java/pipeline/math/GeneratorDistributionException.java
+++
@@ -1,9 +0,0 @@
-package pipeline.math;
-
-public class GeneratorDistributionException extends RuntimeException {
-  private static final long serialVersionUID = -8621858914784704195L;
-
-	public GeneratorDistributionException(String msg) {
-    super(msg);
-  }
-}
