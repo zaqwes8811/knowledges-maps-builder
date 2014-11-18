dbace5fb272f1f7d76328be6156445c2d02d9164
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-18 19:33:12 +0300

    fix bug on move Gson -> Jackson

--- impls/vcards-gae-maven/src/main/java/web_relays/protocols/WordDataValue.java
+++ impls/vcards-gae-maven/src/main/java/web_relays/protocols/WordDataValue.java
@@ -11,12 +11,23 @@ public class WordDataValue {
 	public final String word;  // хорошо бы Optional, но скорее всего не сереализуется
 	public final ArrayList<String> sentences;
 	public final Integer pointPos;
-	public Integer importance = 0;
+
+	public Integer getImportance() {
+		return importance;
+	}
+
+	private Integer importance = 0;
+
+	//public Integer getImportance
 	
 	public void setImportance(Integer val) {
 		this.importance = val;
 	}
-	
+
+	public Integer getMaxImportance() {
+		return maxImportance;
+	}
+
 	private Integer maxImportance = 0;
 	
 	public void setMaxImportance(Integer val) {
