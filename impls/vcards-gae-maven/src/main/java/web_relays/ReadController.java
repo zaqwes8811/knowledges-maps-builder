7d391880e94973b3088b919ddaed92d0f5ab1d62
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-19 13:42:17 +0300

    deployed worked

--- impls/vcards-gae-maven/src/main/java/web_relays/ReadController.java
+++ impls/vcards-gae-maven/src/main/java/web_relays/ReadController.java
@@ -1,6 +1,8 @@
 package web_relays;
 
+import com.google.common.base.Optional;
 import gae_store_space.AppInstance;
+import gae_store_space.PageKind;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pipeline.TextPipeline;
 import web_relays.protocols.PageSummaryValue;
+import web_relays.protocols.PathValue;
+import web_relays.protocols.WordDataValue;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +30,28 @@ public class ReadController {
 	public @ResponseBody
 	List<PageSummaryValue> get(HttpServletRequest request, HttpServletResponse res) {
 		return app.getUserInformation(TextPipeline.defaultUserId);
+	}
+
+	@RequestMapping(value="/pkg", method = RequestMethod.GET, headers="Accept=application/json")
+	public @ResponseBody
+	WordDataValue getterSingleWord(HttpServletRequest request, HttpServletResponse res) {
+		String value = request.getParameter("arg0");
+
+		if (value == null)
+			throw new IllegalArgumentException();
+
+		try {
+			PathValue path = new ObjectMapper().readValue(value, PathValue.class);
+
+			Optional<PageKind> p = app.getPage(path.getPageName().get());
+			if (!p.isPresent())
+				throw new IllegalStateException();
+
+			return p.get().getWordData().get();
+		} catch (IOException ex) {
+			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+
+		return null;
 	}
 }
