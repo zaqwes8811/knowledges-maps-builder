646151f0e332c652821da5c09dbe5c036168ff0d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-24 19:14:10 +0300

    save

--- impls/vcards-gae-maven/src/main/webapp/js/dal.js
+++ impls/vcards-gae-maven/src/main/webapp/js/dal.js
@@ -75,14 +75,19 @@ DataAccessLayer.prototype.getDistributionAsync = function (callback, arg0) {
     .error(function(data) { self.onError(data); });
 }
 
-DataAccessLayer.prototype.getUserSummary = function (callback) {
+DataAccessLayer.prototype.getUserSummary = function (successHandler) {
   var self = this;
   // Get user data
   // Нужно по имени страницы получать список генераторов
   var uri = '/user_summary';
+
+  var errorHandler = function(data) { 
+    self.onError(data); 
+  };
+
   $.get(uri)
-    .success(callback)
-    .error(function(data) { self.onError(data); });
+    .success(successHandler)
+    .error(errorHandler);
 }
 
 DataAccessLayer.prototype.resetFullStore =  function () {
