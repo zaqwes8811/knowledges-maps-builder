a58f02205dfb49fead166ece5294facf42e877b3
zaqwes8811 <igor.a.lugansky@gmail.com>
2019-03-08 13:27:12 +0300

    hw from java fx

---
+++ vc-jfx2-mvn2/src/main/java/MainApp.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// intall editor
+// https://www.oracle.com/technetwork/java/javase/downloads/javafxscenebuilder-1x-archive-2199384.html
+// java -jar target/vc-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/hello.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
--- vocabulary-cards-gae-mvn2/run.sh
+++ vocabulary-cards-gae-mvn2/run.sh
@@ -3,12 +3,15 @@
 ROOT="/mnt/d2/voc-cards-test-data/Merlin - season 1.en/"
 export STR_FILENAME="$ROOT/Merlin - 1x08 - The Beginning Of The End.PDTV.AFFiNiTY.en.srt"
 
+# FIXME: need deep regex knowl.
+
 # export PATTERN="('ve)|(have)"
 # export PATTERN="('ve been)"
 # export PATTERN="('ve been)"
 # export PATTERN="(either)"
 # export PATTERN="(used to)"
-export PATTERN="(would)|(wouldn't)"
+# export PATTERN="(would)|(wouldn't)"
+export PATTERN="(ed )"
 
 # FIXME: faster!
 #mvn test -Dtest=SpecWordsApp.select
